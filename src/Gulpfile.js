0f709dd4ef73b8c4bf6feda473850905d099d3e0
Lukas Hechenberger <admin@ls-age.com>
2016-12-07 11:19:16 +0100

    Added dummy test

---
+++ src/Gulpfile.js
@@ -0,0 +1,1 @@
+import { src, dest } from 'gulp';
---
+++ test/Gulpfile.spec.js
@@ -0,0 +1,9 @@
+import expect from 'unexpected';
+
+import * as tasks from '../src/Gulpfile';
+
+describe('test', function() {
+  it('should be run', function() {
+    expect(true, 'to be true');
+  });
+});
