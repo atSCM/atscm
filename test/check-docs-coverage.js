e11df0460fc60eaf726ef31d4e8a9601af88b30b
Lukas Hechenberger <admin@ls-age.com>
2016-12-11 15:02:52 +0100

    Create docs coverage XML report

--- test/check-docs-coverage.js
+++ test/check-docs-coverage.js
@@ -1,21 +1,38 @@
 const joinPath = require('path').join;
+const { writeFileSync } = require('fs');
+const args = require('yargs').argv;
 const esdocConfig = require('../esdoc.json');
 
-// Get ESDoc-generated coverage.json
-const docsCoveragePath = joinPath('../', esdocConfig.destination, 'coverage.json');
-const docsCoverage = require(docsCoveragePath);
-
 // Configuration
-const requiredPercentage = 90;
+const RequiredPercentage = 90;
+
+// Paths
+const docsPath = joinPath('../', esdocConfig.destination);
+const docsCoveragePath = joinPath(docsPath, 'coverage.json');
+const docsReportPath = args._.length >= 0 ? args._[0] : undefined;
+
+// Get ESDoc-generated coverage.json
+const docsCoverage = require(docsCoveragePath);
 
 // Get docs coverage
 const givenPercentage = parseInt(/([0-9]+)%/.exec(docsCoverage.coverage)[1], 10);
 
 // Report docs coverage
-const msg = `Documentation coverage is at ${docsCoverage.coverage}`;
-if (givenPercentage >= requiredPercentage) {
-  process.stdout.write(`${msg}.\n`);
-} else {
-  process.stderr.write(`${msg}, ${requiredPercentage}% required.\n`);
+const fail = givenPercentage < RequiredPercentage;
+const msg = `Coverage is at ${docsCoverage.coverage} (${RequiredPercentage}% required)`;
+
+if (docsReportPath) { // Create JUnit XML Report
+  const report = `<testsuite tests="1">
+  <testcase classname="ESDoc" name="Coverage">${
+      fail ? `\n    <failure type="NotEnoughCoverage">${msg}</failure>\n` : ''
+    }  </testcase>
+</testsuite>`;
+
+  writeFileSync(docsReportPath, report);
+}
+
+process.stdout.write(`${msg}\n`);
+
+if (fail) {
   process.exit(1);
 }
