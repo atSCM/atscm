828391f3738a487910cda29024a0a0e4efc7fae6
Lukas Hechenberger <admin@ls-age.com>
2016-12-13 10:13:48 +0100

    Initial commit

--- packages/atscm-cli/test/bin/atscm.spec.js
+++ packages/atscm-cli/test/bin/atscm.spec.js
@@ -1,3 +1,42 @@
-/**
- * Created by lukas on 12/12/2016.
- */
+import expect from 'unexpected';
+import proxyquire from 'proxyquire';
+
+describe('bin/atscm', function() {
+  class StubCli {
+
+    constructor(...args) {
+      if (!StubCli.constructorCalled) {
+        StubCli.constructorCalled = 0;
+      }
+
+      StubCli.constructorCalled++;
+      StubCli.constructorCalledWith = args;
+    }
+
+    launch(...args) {
+      if (!StubCli.launchCalled) {
+        StubCli.launchCalled = 0;
+      }
+
+      StubCli.launchCalled++;
+      StubCli.launchCalledWith = args;
+    }
+
+  }
+
+  proxyquire('../../src/bin/atscm', {
+    '../': {
+      __esModule: true,
+      default: StubCli,
+    },
+  });
+
+  it('should call launch', function() {
+    require('../../src/bin/atscm'); // eslint-disable-line global-require
+
+    expect(StubCli.constructorCalled, 'to equal', 1);
+    expect(StubCli.constructorCalledWith, 'to equal', [process.argv]);
+    expect(StubCli.launchCalled, 'to equal', 1);
+    expect(StubCli.launchCalledWith, 'to equal', []);
+  });
+});
