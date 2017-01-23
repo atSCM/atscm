f6ab40f5cc2578c6980a448730cff93b46546896
Lukas Hechenberger <admin@ls-age.com>
2017-01-23 17:40:09 +0100

    Initial commit

---
+++ packages/atscm-cli/src/typedef/external.js
@@ -0,0 +1,15 @@
+/**
+ * @external {liftoff~Liftoff} https://github.com/js-cli/js-liftoff
+ */
+
+/**
+ * @external {gulplog} https://github.com/gulpjs/gulplog
+ */
+
+/**
+ * @external {chalk} https://github.com/chalk/chalk
+ */
+
+/**
+ * @external {yargs} http://yargs.js.org
+ */
