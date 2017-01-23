a16c28007fa6331b1e3fa7dc981b0421b1e41224
Lukas Hechenberger <admin@ls-age.com>
2017-01-23 16:41:08 +0100

    Renamed Logger file

---
+++ packages/atscm-cli/test/lib/util/Logger.spec.js
@@ -0,0 +1,205 @@
+/* eslint-disable no-console */
+import expect from 'unexpected';
+import { stub } from 'sinon';
+
+import gulplog from 'gulplog';
+import Logger from '../../../src/lib/util/Logger';
+
+/** @test {LogFormat} */
+describe('LogFormat', function() {
+  const text = 'Test string';
+
+  function expectStyle(styledText, style) {
+    expect(styledText, 'to equal', `${style.open}${text}${style.close}`);
+  }
+
+  /** @test {LogFormat.path} */
+  describe('.path', function() {
+    it('should change color to magenta', function() {
+      expectStyle(Logger.format.path(text), Logger.colors.styles.magenta);
+    });
+
+    it('should prettify user path', function() {
+      expect(Logger.format.path(process.env.HOME), 'to contain', '~');
+    });
+  });
+
+  /** @test {LogFormat.command} */
+  describe('.command', function() {
+    it('should return bold text', function() {
+      expectStyle(Logger.format.command(text), Logger.colors.styles.bold);
+    });
+  });
+
+  /** @test {LogFormat.value} */
+  describe('.value', function() {
+    it('should return cyan colored text', function() {
+      expectStyle(Logger.format.value(text), Logger.colors.styles.cyan);
+    });
+  });
+
+  /** @test {LogFormat#number} */
+  describe('#number', function() {
+    it('should return magenta colored text', function() {
+      expectStyle(Logger.format.number(text), Logger.colors.styles.magenta);
+    });
+  });
+});
+
+/** @test {Logger} */
+describe('Logger', function() {
+  function getText() {
+    return `Text ${(new Date())}`;
+  }
+
+  let debugSpy;
+  let infoSpy;
+  let warnSpy;
+  let errorSpy;
+
+  before(function() {
+    Logger.applyOptions({ });
+  });
+
+  beforeEach(function() {
+    debugSpy = stub(gulplog, 'debug');
+    infoSpy = stub(gulplog, 'info');
+    warnSpy = stub(gulplog, 'warn');
+    errorSpy = stub(gulplog, 'error');
+  });
+
+  afterEach(function() {
+    gulplog.debug.restore();
+    gulplog.info.restore();
+    gulplog.warn.restore();
+    gulplog.error.restore();
+  });
+
+  /** @test {Logger.debug} */
+  describe('.debug', function() {
+    it('should call gulplog.debug', function() {
+      const t = getText();
+      Logger.debug(t);
+
+      expect(debugSpy.calledOnce, 'to be', true);
+      expect(debugSpy.lastCall.args, 'to equal', [t]);
+    });
+  });
+
+  /** @test {Logger.info} */
+  describe('.info', function() {
+    it('should call gulplog.info', function() {
+      const t = getText();
+      Logger.info(t);
+
+      expect(infoSpy.calledOnce, 'to be', true);
+      expect(infoSpy.lastCall.args, 'to equal', [t]);
+    });
+  });
+
+  /** @test {Logger.warn} */
+  describe('.warn', function() {
+    it('should call gulplog.warn', function() {
+      const t = getText();
+      Logger.warn(t);
+
+      expect(warnSpy.calledOnce, 'to be', true);
+      expect(warnSpy.lastCall.args, 'to equal', [t]);
+    });
+  });
+
+  /** @test {Logger.warn} */
+  describe('.error', function() {
+    it('should call gulplog.error', function() {
+      const t = getText();
+      Logger.error(t);
+
+      expect(errorSpy.calledOnce, 'to be', true);
+
+      expect(errorSpy.lastCall.args, 'to equal', [t]);
+    });
+  });
+});
+
+/** @test {Logger} */
+describe.skip('Logger', function() {
+  function getText() {
+    return `Text ${(new Date())}`;
+  }
+
+  before(function() {
+    Logger.applyOptions({ });
+  });
+
+  let logSpy;
+  let warnSpy;
+  let errorSpy;
+
+  beforeEach(function() {
+    logSpy = stub(console, 'log');
+    // warnSpy = stub(gulplog, 'warn');
+    errorSpy = stub(console, 'error');
+  });
+
+  afterEach(function() {
+    logSpy.restore();
+    // warnSpy.restore();
+    errorSpy.restore();
+  });
+
+  /** @test {Logger.debug} */
+  describe('.debug', function() {
+    it('should not be printed by default', function() {
+      const t = getText();
+      Logger.debug(t);
+
+      expect(logSpy.calledOnce, 'to be false');
+    });
+  });
+
+  /** @test {Logger.info} */
+  describe('.info', function() {
+    it('should be printed', function() {
+      const t = getText();
+      Logger.info(t);
+
+      // console.log('CALL COUNT', logSpy.callCount);
+
+      expect(logSpy.calledOnce, 'to be true');
+      expect(logSpy.lastCall.args[0], 'to equal', t);
+    });
+  });
+
+  /** @test {Logger.warn} */
+  describe('.warn', function() {
+    it('should be printed', function() {
+      const t = getText();
+      Logger.warn(t);
+
+      expect(logSpy.calledOnce, 'to be true');
+      expect(warnSpy.lastCall.args[0], 'to equal', t);
+    });
+  });
+
+  /** @test {Logger.error} */
+  describe('.error', function() {
+    it('should be printed using stderr', function() {
+      const t = getText();
+      Logger.error(t);
+
+      expect(logSpy.calledOnce, 'to be true');
+      expect(errorSpy.lastCall.args[0], 'to equal', t);
+    });
+  });
+
+  /** @test {Logger.applyOptions} */
+  describe('.applyOptions', function() {
+    it('should apply options to gulplog', function() {
+      Logger.applyOptions({ silent: true });
+
+      Logger.error('Test');
+
+      expect(errorSpy.calledOnce, 'to be false');
+    });
+  });
+});
