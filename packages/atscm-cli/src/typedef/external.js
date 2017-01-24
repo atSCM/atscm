8af92bd3469a475808e0c3e80ddecfb5bfc2c922
Lukas Hechenberger <admin@ls-age.com>
2017-01-24 09:15:48 +0100

    Added Liftoff.Environment

--- packages/atscm-cli/src/typedef/external.js
+++ packages/atscm-cli/src/typedef/external.js
@@ -1,5 +1,15 @@
 /**
- * @external {liftoff~Liftoff} https://github.com/js-cli/js-liftoff
+ * @external {Liftoff} https://github.com/js-cli/js-liftoff
+ */
+
+/**
+ * A {@link Liftoff} environment.
+ * @typedef {Object} Liftoff.Environment
+ * @property {String} cwd The resulting working directory.
+ * @property {String} [configPath] Path to the config file found.
+ * @property {String} [configBase] The directory the config file is in.
+ * @property {String} [modulePath] Path to the local module found.
+ * @property {Object} [modulePackage={}] The parsed package.json of the local module found.
  */
 
 /**
