3977d240a02334b5c0487428fb7b12116f5b1d3d
Lukas Hechenberger <admin@ls-age.com>
2017-01-30 11:15:03 +0100

    Add tests

--- packages/atscm-cli/test/cli/commands/Config.spec.js
+++ packages/atscm-cli/test/cli/commands/Config.spec.js
@@ -1,14 +1,32 @@
 import expect from 'unexpected';
+import { stub } from 'sinon';
 
 import ConfigCommand from '../../../src/cli/commands/Config';
+import Logger from '../../../src/lib/util/Logger';
 
 /** @test {ConfigCommand} */
 describe('ConfigCommand', function() {
   /** @test {ConfigCommand#run} */
   describe('#run', function() {
-    it('is not implemented yet', function() {
-      expect(() => (new ConfigCommand('config', 'Validate and print the config file.')).run(),
-        'to throw error', 'Not implemented yet');
+    beforeEach(function() {
+      stub(Logger, 'info');
+    });
+
+    afterEach(function() {
+      Logger.info.restore();
+    });
+
+    const command = new ConfigCommand('config', 'Print configuration.');
+
+    it('should print the current configuration', function() {
+      command.run({
+        environment: {
+          configPath: __filename,
+        },
+      });
+
+      expect(Logger.info.calledOnce, 'to be', true);
+      expect(Logger.info.lastCall.args[0], 'to match', new RegExp('^Configuration at'));
     });
   });
 });
