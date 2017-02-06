702f055eafffddc67ef85ed8601625579597e6e0
Lukas Hechenberger <admin@ls-age.com>
2017-02-06 11:08:19 +0100

    Document node-opcua types

---
+++ src/typedef/external/node-opcua.js
@@ -0,0 +1,11 @@
+/**
+ * @external {NodeOpcua} http://node-opcua.github.io/api_doc/
+ */
+
+/**
+ * @external {NodeOpcua.NodeId} http://node-opcua.github.io/api_doc/classes/NodeId.html
+ */
+
+/**
+ * @external {NodeOpcua.NodeIdType} http://node-opcua.github.io/api_doc/classes/NodeIdType.html
+ */
