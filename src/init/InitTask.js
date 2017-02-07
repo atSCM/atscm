dc9dbdeb8a6327d26aafdace04c68131bb39073c
Lukas Hechenberger <admin@ls-age.com>
2017-02-07 13:27:47 +0100

    Use global import

--- src/init/InitTask.js
+++ src/init/InitTask.js
@@ -3,6 +3,7 @@ import { src, dest } from 'gulp';
 import handlebars from 'gulp-compile-handlebars';
 import helpers from 'handlebars-helpers';
 import streamToPromise from 'stream-to-promise';
+import deps from '../../res/init/templates/dependencies.json';
 
 export default class InitTask {
 
@@ -18,9 +19,7 @@ export default class InitTask {
   static run(options) {
     const langId = options.configLang;
 
-    // eslint-disable-next-line global-require
-    const install = require(join(__dirname, '../../res/init/templates/dependencies.json'))
-      .lang[langId];
+    const install = deps.lang[langId];
 
     const stream = src(this.filesToHandle(langId))
       .pipe(handlebars(options, {
