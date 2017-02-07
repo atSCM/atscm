f429a609b79549341da566cf1176ab65971baba6
Lukas Hechenberger <admin@ls-age.com>
2017-02-07 13:19:51 +0100

    Moved InitTask class to seperate file

---
+++ src/init/InitTask.js
@@ -0,0 +1,35 @@
+import { join } from 'path';
+import { src, dest } from 'gulp';
+import handlebars from 'gulp-compile-handlebars';
+import helpers from 'handlebars-helpers';
+import streamToPromise from 'stream-to-promise';
+
+export class InitTask {
+
+  static filesToHandle(langId) {
+    return [
+      './general/**/*',
+      './general/**/.*',
+      `./lang/${langId}/**/*.*`,
+      `./lang/${langId}/**/.*`,
+    ].map(p => join(__dirname, '../../res/init/templates', p));
+  }
+
+  static run(options) {
+    const langId = options.configLang;
+
+    // eslint-disable-next-line global-require
+    const install = require(join(__dirname, '../../res/init/templates/dependencies.json'))
+      .lang[langId];
+
+    const stream = src(this.filesToHandle(langId))
+      .pipe(handlebars(options, {
+        helpers: helpers(),
+      }))
+      .pipe(dest('./'));
+
+    return streamToPromise(stream)
+      .then(() => ({ install }));
+  }
+
+}
