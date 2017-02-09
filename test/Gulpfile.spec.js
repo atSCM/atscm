ac858a40905f292ae439c23f17d916471bf46a1f
Lukas Hechenberger <admin@ls-age.com>
2017-02-09 10:08:46 +0100

    Test task descriptions

---
+++ test/Gulpfile.spec.js
@@ -0,0 +1,14 @@
+import expect from 'unexpected';
+
+import * as tasks from '../src/Gulpfile';
+
+describe('Gulpfile', function() {
+  it('should export description for all tasks', function() {
+    Object.keys(tasks).forEach(name => {
+      const desc = tasks[name].description;
+
+      expect(desc, 'to be defined');
+      expect(desc, 'not to be empty');
+    });
+  });
+});
