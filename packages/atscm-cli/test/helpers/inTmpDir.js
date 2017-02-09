80edfe37789bd64862b1d49df3935d24aae0eb00
Lukas Hechenberger <admin@ls-age.com>
2017-02-09 08:20:01 +0100

    Add inTmpDir test helper

---
+++ packages/atscm-cli/test/helpers/inTmpDir.js
@@ -0,0 +1,16 @@
+import { mkdir } from 'fs';
+import { join } from 'path';
+import rmdir from 'rimraf';
+
+/**
+ * Creates a new temporary directory and runs `run` with it's path.
+ * @param {function(path: String)} run The action to run in the temporary directory.
+ */
+export default function inTmpDir(run) {
+  const path = join(__dirname, `../tmp_${(new Date()).getTime()}`);
+
+  before(done => mkdir(path, done));
+  after(done => rmdir(path, done));
+
+  run(path);
+}
