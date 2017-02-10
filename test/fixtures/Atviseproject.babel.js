3c75b47ee9612c2361b6dc38124bfb8c3ed2dfb1
Lukas Hechenberger <admin@ls-age.com>
2017-02-10 10:46:15 +0100

    Prepare for tests on demo server

---
+++ test/fixtures/Atviseproject.babel.js
@@ -0,0 +1,23 @@
+import { Atviseproject } from '../../';
+
+export default class TestProject extends Atviseproject {
+
+  static get host() {
+    return '185.67.228.66';
+  }
+
+  static get port() {
+    return {
+      opc: 4888,
+      http: 8888,
+    };
+  }
+
+  static get login() {
+    return {
+      username: process.env.ATVISE_USERNAME,
+      password: process.env.ATVISE_PASSWORD,
+    };
+  }
+
+}
