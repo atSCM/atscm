8a86d9d10da37def5c8d38bb155abe96e17e91d0
Lukas Hechenberger <admin@ls-age.com>
2017-02-12 19:17:25 +0100

    Add missing module [ci skip]

---
+++ src/config/ProjectConfig.js
@@ -0,0 +1,12 @@
+/**
+ * The path to the project's configuration file.
+ */
+export const path = process.env.ATSCM_CONFIG_PATH;
+
+/**
+ * The current project's configuration.
+ * @type {Atviseproject}
+ */
+const config = require(path).default;
+
+export default config;
