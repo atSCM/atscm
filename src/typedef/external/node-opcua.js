eb3d76481a25432d221d7586ac53a8c3d7899aaa
Lukas Hechenberger <admin@ls-age.com>
2017-02-12 19:47:10 +0100

    Document node-opcua's OPCUAClient and ClientSession

--- src/typedef/external/node-opcua.js
+++ src/typedef/external/node-opcua.js
@@ -9,3 +9,11 @@
 /**
  * @external {NodeOpcua.NodeIdType} http://node-opcua.github.io/api_doc/classes/NodeIdType.html
  */
+
+/**
+ * @external {NodeOpcua.OPCUAClient} http://node-opcua.github.io/api_doc/classes/OPCUAClient.html
+ */
+
+/**
+ * @external {NodeOpcua.ClientSession} http://node-opcua.github.io/api_doc/classes/ClientSession.html
+ */
