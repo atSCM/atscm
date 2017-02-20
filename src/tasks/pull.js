20fc7e6d54dac821c2ddaace3fdc67262ff252f5
Lukas Hechenberger <admin@ls-age.com>
2017-02-20 13:44:13 +0100

    Implement pull task

--- src/tasks/pull.js
+++ src/tasks/pull.js
@@ -1,5 +1,38 @@
+import { dest } from 'gulp';
+import ProjectConfig from '../config/ProjectConfig';
+import NodeStream from '../lib/server/NodeStream';
+import ReadStream from '../lib/server/ReadStream';
+import { TransformDirection } from '../lib/transform/Transformer';
+import MappingStream from '../lib/transform/Mapping';
+
+/**
+ * Pulls all nodes from atvise server.
+ */
 export default function pull() {
-  return Promise.reject(new Error('Not implemented: Run pull'));
+  let found = 0;
+  let pulled = 0;
+  let stored = 0;
+  const nodeStream = new NodeStream(ProjectConfig.nodes, { read: true })
+    .on('data', () => found++);
+  const readStream = new ReadStream()
+    .on('data', () => pulled++);
+  const mappingStream = new MappingStream({ direction: TransformDirection.FromDB });
+  const storeStream = dest('./src')
+    .on('data', () => stored++);
+
+  const printProgress = setInterval(() => {
+    process.stdout.write(`\rFound: ${found}, pulled: ${pulled}, stored: ${stored}`);
+  }, 1000);
+
+  return nodeStream
+    .pipe(readStream)
+    .pipe(mappingStream)
+    .pipe(storeStream)
+    .on('end', () => {
+      process.stdout.clearLine();
+      process.stdout.write('\r');
+      clearInterval(printProgress);
+    });
 }
 
-pull.description = 'Not implemented: Push to the atvise server';
+pull.description = 'Pull all nodes from atvise server';
---
+++ test/src/tasks/pull.spec.js
@@ -0,0 +1,50 @@
+import expect from 'unexpected';
+import proxyquire from 'proxyquire';
+
+import { Stream } from 'stream';
+import through, { ctor as throughStreamClass } from 'through2';
+import NodeId from '../../../src/lib/server/NodeId';
+import AtviseFile from '../../../src/lib/server/AtviseFile';
+
+const pull = proxyquire('../../../src/tasks/pull', {
+  '../lib/server/NodeStream': {
+    _esModule: true,
+    default: class StubStream extends throughStreamClass({ objectMode: true }) {
+      constructor() {
+        super();
+
+        this.push({
+          nodeId: new NodeId('AGENT.DISPLAYS.Main'),
+          typeDefinition: new NodeId('VariableTypes.ATVISE.Display'),
+        });
+
+        this.end();
+      }
+    },
+  },
+  gulp: {
+    _esModule: true,
+    dest: () => through.obj(),
+  },
+}).default;
+
+/** @test {pull} */
+describe('pull', function() {
+  this.timeout(5000);
+
+  it('should return a stream', function(done) {
+    const stream = pull();
+    expect(stream, 'to be a', Stream);
+    stream.once('end', done);
+  });
+  
+  it('should stream AtviseFiles', function(done) {
+    const stream = pull();
+
+    stream.on('data', data => {
+      expect(data, 'to be a', AtviseFile);
+    });
+
+    stream.once('end', done);
+  });
+});
