f37a512c634f7ee2789bc67d030f02215e1b4fa2
Lukas Hechenberger <admin@ls-age.com>
2017-02-20 12:35:24 +0100

    Make ESLint happy [ci skip]

--- src/lib/server/AtviseFile.js
+++ src/lib/server/AtviseFile.js
@@ -2,8 +2,8 @@ import File from 'vinyl';
 
 /**
  * An extension to {@link vinyl~File} providing some additional, atvise-related properties.
- * @property {NodeOpcua.DataType} AtviseFile#dataType The {@link NodeOpcua.DataType} the node is stored against
- * on atvise server.
+ * @property {NodeOpcua.DataType} AtviseFile#dataType The {@link NodeOpcua.DataType} the node is
+ * stored against on atvise server.
  * @property {NodeId} typeDefinition The file's type definition on atvise server.
  * FIXME: Additional properties not showing in API docs.
  */
