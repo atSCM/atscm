46841a5a93d78037aedc7e08e646eb6095c6a372
Lukas Hechenberger <admin@ls-age.com>
2017-02-21 08:35:20 +0100

    Move mapping logic to AtviseFile

--- src/lib/server/AtviseFile.js
+++ src/lib/server/AtviseFile.js
@@ -1,4 +1,73 @@
 import File from 'vinyl';
+import { DataType, VariantArrayType, resolveNodeId } from 'node-opcua';
+import Logger from 'gulplog';
+import AtviseTypes from './Types';
+import NodeId from './NodeId';
+
+// Path related cache
+const AtviseTypesByValue = AtviseTypes
+  .reduce((result, type) => Object.assign(result, {
+    [type.typeDefinition.value]: type,
+  }), {});
+
+const AtviseTypesByIdentifier = AtviseTypes
+  .reduce((result, type) => Object.assign(result, {
+    [type.identifier]: type,
+  }), {});
+
+/**
+ * A map providing shorter extensions for data types
+ * @type {Map<NodeOpcua.DataType, String>}
+ */
+export const ExtensionForDataType = {
+  [DataType.Boolean]: 'bool',
+  [DataType.XmlElement]: 'xml',
+};
+
+function reverseObject(obj) {
+  return Object.keys(obj)
+    .reduce((result, key) => Object.assign(result, {
+      [obj[key]]: key,
+    }), {});
+}
+
+/**
+ * A map providing data types for shorter extensions (Reverse of {@link DataTypeForExtension}).
+ * * @type {Map<String, NodeOpcua.DataType>}
+ */
+export const DataTypeForExtension = reverseObject(ExtensionForDataType);
+
+// Cache DataType
+const types = Object.keys(DataType);
+const typeExtensions = types.map(t => t.toLowerCase());
+
+// Cache TypeDefinitions
+const VariableTypeDefinition = new NodeId(NodeId.NodeIdType.NUMERIC, 62, 0);
+const PropertyTypeDefinition = new NodeId(NodeId.NodeIdType.NUMERIC, 68, 0);
+
+// Value encoding related cache
+const Decoder = {
+  [DataType.Boolean]: stringValue => stringValue === 'true',
+  [DataType.String]: stringValue => stringValue,
+  [DataType.NodeId]: stringValue => resolveNodeId(stringValue),
+  [DataType.DateTime]: stringValue => new Date(Number.parseInt(stringValue, 10)),
+};
+
+const Encoder = {
+  [DataType.DateTime]: date => date.getTime().toString(),
+};
+
+/**
+ * Returns the extension for a specific {@link NodeOpcua.DataType}.
+ * Algorithm:
+ *   - if the type has a shortened extension defined in {@link ExtensionForDataType}, return it.
+ *   - else return the DataType's name, in lowercase letters.
+ * @param {NodeOpcua.DataType} dataType The datatype to get the extension for.
+ * @return {String} The resulting extension.
+ */
+function extensionForDataType(dataType) {
+  return ExtensionForDataType[dataType] || dataType.toString().toLowerCase();
+}
 
 /**
  * An extension to {@link vinyl~File} providing some additional, atvise-related properties.
@@ -10,11 +79,260 @@ import File from 'vinyl';
 export default class AtviseFile extends File {
 
   /**
+   * Returns a storage path for a {@link ReadStream.ReadResult}.
+   * @param {ReadStream.ReadResult} readResult The read result to get a path for.
+   */
+  static pathForReadResult(readResult) {
+    let path = readResult.nodeId.filePath;
+
+    const dataType = readResult.value.$dataType;
+    const arrayType = readResult.value.$arrayType;
+    const typeDefinition = readResult.referenceDescription.typeDefinition;
+
+    if (typeDefinition.value === VariableTypeDefinition.value) {
+      // Variable nodes are stored with their lowercase datatype as an extension
+      path += `.${extensionForDataType(dataType)}`;
+    } else if (typeDefinition.value === PropertyTypeDefinition.value) {
+      // Property nodes are stored with ".prop" and their lowercase datatype as an extension
+      path += `.prop.${extensionForDataType(dataType)}`;
+    } else {
+      // Handle object types
+      let identifier = 'obj';
+      let fileExtension = false;
+      let keepExtension = false;
+
+      const atType = AtviseTypesByValue[typeDefinition.value];
+      if (atType) {
+        identifier = atType.identifier;
+        fileExtension = atType.fileExtension;
+        keepExtension = atType.keepExtension;
+      } else {
+        process.stdout.clearLine();
+        Logger.warn('\rMissing', typeDefinition.namespace, typeDefinition.value,
+          `(${readResult.nodeId.value})`);
+      }
+
+      if (!keepExtension) {
+        path += `.${identifier}.${fileExtension || extensionForDataType(dataType)}`;
+      }
+    }
+
+    // Add "array" or "matrix" extensions for corresponding array types
+    if (arrayType !== VariantArrayType.Scalar) {
+      path += `.${arrayType === VariantArrayType.Array ? 'array' : 'matrix'}`;
+    }
+
+    return path;
+  }
+
+  /**
+   * Encodes a node's value to file contents.
+   * @param {*} value The value to encode.
+   * @param {NodeOpcua.DataType} dataType The {@link NodeOpcua.DataType} to encode the value for.
+   * @return {?Buffer} The encoded file contents or null.
+   */
+  static encodeValue(value, dataType) {
+    if (value === null) {
+      return null;
+    }
+
+    const encoder = Encoder[dataType];
+    return Buffer.from(encoder ? encoder(value.value) : value.value.toString());
+  }
+
+  /**
+   * Decodes a file's contents to a node's value
+   * @param {Buffer} buffer The file contents to decode.
+   * @param {NodeOpcua.DataType} dataType The {@link NodeOpcua.DataType} to decode the contents for.
+   * @return {?*} The decoded node value or null.
+   */
+  static decodeValue(buffer, dataType) {
+    if (buffer === null) {
+      return null;
+    }
+
+    const decoder = Decoder[dataType];
+
+    if (decoder) {
+      return decoder(buffer.toString());
+    }
+
+    return buffer;
+  }
+
+  /**
+   * Creates a new {@link AtviseFile} for the given {@link ReadStream.ReadResult}.
+   * @param {ReadStream.ReadResult} readResult The read result to create the file for.
+   * @return {AtviseFile} The resulting file.
+   */
+  static fromReadResult(readResult) {
+    if (!readResult.value) {
+      throw new Error('no value');
+    }
+
+    return new AtviseFile({
+      path: AtviseFile.pathForReadResult(readResult),
+      contents: AtviseFile.encodeValue(readResult.value, readResult.value.$dataType),
+      _dataType: readResult.value.$dataType,
+      _arrayType: readResult.value.$arrayType,
+      _typeDefinition: readResult.referenceDescription.typeDefinition,
+    });
+  }
+
+  /**
+   * Recalculates {@link AtviseFile#dataType}, {@link AtviseFile#arrayType} and
+   * {@link AtviseFile#typeDefinition}. **Never call this method directly.**
+   */
+  _getMetadata() {
+    // Set default metadata
+    /**
+     * The node's stored {@link NodeOpcua.VariantArrayType}.
+     * @type {?NodeOpcua.VariantArrayType}
+     */
+    this._arrayType = VariantArrayType.Scalar;
+
+    let extensions = [];
+    extensions = this.relative.match(/\.(.*)/)[1].split('.');
+
+    function ifLastExtensionMatches(matches, fn) {
+      if (matches(extensions[extensions.length - 1])) {
+        fn(extensions.pop());
+      }
+    }
+
+    const complete = () => {
+      return this._dataType !== undefined && this._typeDefinition !== undefined;
+    };
+
+    // Handle array types
+    ifLastExtensionMatches(ext => ext === 'array', () => {
+      this._arrayType = VariantArrayType.Array;
+    });
+
+    ifLastExtensionMatches(ext => ext === 'matrix', () => {
+      this._arrayType = VariantArrayType.Matrix;
+    });
+
+    ifLastExtensionMatches(ext => typeExtensions.includes(ext), ext => {
+      /**
+       * The node's stored {@link NodeOpcua.DataType}.
+       * @type {?NodeOpcua.DataType}
+       */
+      this._dataType = DataType[types[typeExtensions.indexOf(ext)]];
+    });
+
+    // Handle wrapped data types (e.g. "bool" for DataType.Boolean)
+    ifLastExtensionMatches(ext => DataTypeForExtension[ext], ext => {
+      this._dataType = DataType[DataTypeForExtension[ext]];
+    });
+
+    if (extensions.length === 0) { // Got variable
+      /**
+       * The node's stored type definition.
+       * @type {?NodeOpcua.NodeId}
+       */
+      this._typeDefinition = new NodeId(NodeId.NodeIdType.NUMERIC, 62, 0);
+    }
+
+    ifLastExtensionMatches(ext => ext === 'prop', () => {
+      this._typeDefinition = new NodeId(NodeId.NodeIdType.NUMERIC, 68, 0);
+    });
+
+    let gotCustomType = false;
+    ifLastExtensionMatches(ext => ext === 'obj', () => {
+      gotCustomType = true;
+
+      Logger.warn('NEED SPECIAL HANDLING');
+    });
+
+    if (!gotCustomType && !complete()) {
+      // Handle atvise types
+      let foundAtType = false;
+
+      Object.keys(AtviseTypesByIdentifier).forEach(identifier => {
+        if (!foundAtType && extensions.includes(identifier)) {
+          foundAtType = true;
+          const type = AtviseTypesByIdentifier[identifier];
+
+          this._typeDefinition = type.typeDefinition;
+          this._dataType = type.dataType;
+        }
+      });
+    }
+
+    if (!gotCustomType && !complete()) {
+      Logger.warn('FALLING BACK TO OCTET STREAM');
+      this._typeDefinition = new NodeId('VariableTypes.ATVISE.Resource.OctetStream');
+      this._dataType = DataType.ByteString;
+    }
+
+    if (!complete()) {
+      Logger.warn('Unable to map', this.relative);
+    }
+  }
+
+  /**
+   * The file's {@link NodeOpcua.DataType}.
+   * @type {NodeOpcua.DataType}
+   */
+  get dataType() {
+    if (!this._dataType) {
+      this._getMetadata();
+    }
+
+    return this._dataType;
+  }
+
+  /**
+   * The file's {@link NodeOpcua.VariantArrayType}.
+   * @type {NodeOpcua.VariantArrayType}
+   */
+  get arrayType() {
+    if (!this._arrayType) {
+      this._getMetadata();
+    }
+
+    return this._arrayType;
+  }
+
+  /**
+   * The file's type definition.
+   * @type {NodeOpcua.NodeId}
+   */
+  get typeDefinition() {
+    if (!this._typeDefinition) {
+      this._getMetadata();
+    }
+
+    return this._typeDefinition;
+  }
+
+  /**
    * `true` for files containing atvise displays.
    * @type {Boolean}
    */
   get isDisplay() {
     return this.typeDefinition.value === 'VariableTypes.ATVISE.Display';
+  }
+
+  /**
+   * Sets the node value for the file.
+   * @param {?*} newValue The value to set.
+   */
+  set value(newValue) {
+    /**
+     * The file's contents.
+     * @type {?Buffer}
+     */
+    this.contents = AtviseFile.encodeValue(newValue, this.dataType);
+  }
+
+  /**
+   * Returns the decoded node value for the file.
+   * @return {?*} The file's decoded value.
+   */
+  get value() {
+    return AtviseFile.decodeValue(this.contents, this.dataType);
   }
 
 }
--- test/src/lib/server/AtviseFile.spec.js
+++ test/src/lib/server/AtviseFile.spec.js
@@ -1,7 +1,11 @@
 import expect from 'unexpected';
+import { stub } from 'sinon';
 
+import { Buffer } from 'buffer';
 import File from 'vinyl';
+import { DataType, VariantArrayType } from 'node-opcua';
 import AtviseFile from '../../../../src/lib/server/AtviseFile';
+import AtviseTypes from '../../../../src/lib/server/Types';
 import NodeId from '../../../../src/lib/server/NodeId';
 
 /** @test {AtviseFile} */
@@ -15,13 +19,250 @@ describe('AtviseFile', function() {
     });
   });
 
+  const tests = [
+    {
+      name: 'should store variables with their data type as an extension',
+      nodeId: new NodeId('ns=1;s=AGENT.OBJECTS.Test'),
+      dataType: DataType.UInt16,
+      typeDefinition: new NodeId(NodeId.NodeIdType.NUMERIC, 62, 0),
+      arrayType: VariantArrayType.Scalar,
+      filePath: 'AGENT/OBJECTS/Test.uint16',
+    },
+    {
+      name: 'should store variable arrays with their data type as an extension',
+      nodeId: new NodeId('ns=1;s=AGENT.OBJECTS.Test'),
+      dataType: DataType.UInt16,
+      typeDefinition: new NodeId(NodeId.NodeIdType.NUMERIC, 62, 0),
+      arrayType: VariantArrayType.Array,
+      filePath: 'AGENT/OBJECTS/Test.uint16.array',
+    },
+    {
+      name: 'should store variable matrices with their data type as an extension',
+      nodeId: new NodeId('ns=1;s=AGENT.OBJECTS.Test'),
+      dataType: DataType.UInt16,
+      typeDefinition: new NodeId(NodeId.NodeIdType.NUMERIC, 62, 0),
+      arrayType: VariantArrayType.Matrix,
+      filePath: 'AGENT/OBJECTS/Test.uint16.matrix',
+    },
+    {
+      name: 'should store property variables with their data type as an extension',
+      nodeId: new NodeId('ns=1;s=AGENT.OBJECTS.Test.property'),
+      dataType: DataType.UInt16,
+      typeDefinition: new NodeId(NodeId.NodeIdType.NUMERIC, 68, 0),
+      arrayType: VariantArrayType.Scalar,
+      filePath: 'AGENT/OBJECTS/Test/property.prop.uint16',
+    },
+    {
+      name: 'should store html help documents with a ".help.html" extension',
+      nodeId: new NodeId('ns=1;s=AGENT.DISPLAYS.Test.en'),
+      dataType: DataType.XmlElement,
+      typeDefinition: new NodeId('VariableTypes.ATVISE.HtmlHelp'),
+      arrayType: VariantArrayType.Scalar,
+      filePath: 'AGENT/DISPLAYS/Test/en.help.html',
+    },
+    {
+      name: 'should store quickdynamics with a ".qd.xml" extension',
+      nodeId: new NodeId('ns=1;s=SYSTEM.LIBRARY.PROJECT.QUICKDYNAMICS.Test'),
+      dataType: DataType.XmlElement,
+      typeDefinition: new NodeId('VariableTypes.ATVISE.QuickDynamic'),
+      arrayType: VariantArrayType.Scalar,
+      filePath: 'SYSTEM/LIBRARY/PROJECT/QUICKDYNAMICS/Test.qd.xml',
+    },
+    {
+      name: 'should store scripts with a ".script.xml" extension',
+      nodeId: new NodeId('ns=1;s=SYSTEM.LIBRARY.PROJECT.SERVERSCRIPTS.Test'),
+      dataType: DataType.XmlElement,
+      typeDefinition: new NodeId('VariableTypes.ATVISE.ScriptCode'),
+      arrayType: VariantArrayType.Scalar,
+      filePath: 'SYSTEM/LIBRARY/PROJECT/SERVERSCRIPTS/Test.script.xml',
+    },
+    {
+      name: 'should store displays with a ".display.xml" extension',
+      nodeId: new NodeId('ns=1;s=AGENT.DISPLAYS.Test'),
+      dataType: DataType.XmlElement,
+      typeDefinition: new NodeId('VariableTypes.ATVISE.Display'),
+      arrayType: VariantArrayType.Scalar,
+      filePath: 'AGENT/DISPLAYS/Test.display.xml',
+    },
+    {
+      name: 'should store scripts with a ".locs.xml" extension',
+      nodeId: new NodeId('ns=1;s=SYSTEM.LIBRARY.PROJECT.de'),
+      dataType: DataType.XmlElement,
+      typeDefinition: new NodeId('VariableTypes.ATVISE.TranslationTable'),
+      arrayType: VariantArrayType.Scalar,
+      filePath: 'SYSTEM/LIBRARY/PROJECT/de.locs.xml',
+    },
+    {
+      name: 'should store custom resources with their original extension',
+      nodeId: new NodeId('ns=1;s=SYSTEM.LIBRARY.PROJECT.RESOURCES/Test.md'),
+      dataType: DataType.ByteString,
+      typeDefinition: new NodeId('VariableTypes.ATVISE.Resource.OctetStream'),
+      arrayType: VariantArrayType.Scalar,
+      filePath: 'SYSTEM/LIBRARY/PROJECT/RESOURCES/Test.md',
+    },
+  ].concat(
+    AtviseTypes.filter(t => t.constructor.name === 'AtviseResourceType')
+      .map(t => ({
+        name: `should store ${t.typeDefinition.value} resources with their original extension`,
+        nodeId: new NodeId(`ns=1;s=SYSTEM.LIBRARY.PROJECT.RESOURCES/Test.${t.identifier}`),
+        dataType: DataType.ByteString,
+        typeDefinition: new NodeId(t.typeDefinition.value),
+        arrayType: VariantArrayType.Scalar,
+        filePath: `SYSTEM/LIBRARY/PROJECT/RESOURCES/Test.${t.identifier}`,
+      }))
+  );
+
+  /** @test {AtviseFile.pathForReadResults} */
+  describe('.pathForReadResults', function() {
+    tests.forEach(test => {
+      it(test.name, function() {
+        expect(AtviseFile.pathForReadResult({
+          nodeId: test.nodeId,
+          value: {
+            $dataType: test.dataType,
+            $arrayType: test.arrayType,
+          },
+          referenceDescription: {
+            typeDefinition: test.typeDefinition,
+          },
+        }), 'to equal', test.filePath);
+      });
+    })
+  });
+
+  /** @test {AtviseFile.encodeValue} */
+  describe('.encodeValue', function() {
+    it('should forward null', function() {
+      expect(AtviseFile.encodeValue(null), 'to equal', null);
+    });
+
+    it('should store timestamp as string for DateTime values', function() {
+      const now = new Date();
+
+      expect(AtviseFile.encodeValue({ value: now }, DataType.DateTime),
+        'to equal', Buffer.from(now.getTime().toString()));
+    });
+  });
+
+  /** @test {AtviseFile.decodeValue} */
+  describe('.decodeValue', function() {
+    it('should forward null', function() {
+      expect(AtviseFile.decodeValue(null), 'to equal', null);
+    });
+
+    function testDecoderForDataType(dataType, rawValue, expectedValue) {
+      it(`decoder for ${dataType} should work`, function() {
+        expect(AtviseFile.decodeValue(Buffer.from(rawValue), dataType),
+          'to satisfy', expectedValue);
+      });
+    }
+
+    const now = (new Date());
+
+    [
+      [DataType.Boolean, 'false', false],
+      [DataType.Boolean, 'true', true],
+      [DataType.String, 'test', 'test'],
+      [DataType.NodeId, 'ns=1;s=AGENT.DISPLAYS.Main', new NodeId('AGENT.DISPLAYS.Main')],
+      [DataType.DateTime, now.getTime().toString(), now],
+    ].forEach(t => testDecoderForDataType(...t));
+
+    it('should forward buffer without spection encoding', function() {
+      const buffer = new Buffer('test');
+      expect(AtviseFile.decodeValue(buffer, DataType.ByteString), 'to equal', buffer);
+    });
+  });
+
+  /** @test {AtviseFile.fromReadResult} */
+  describe('.fromReadResult', function() {
+    it('should fail without value', function() {
+      expect(() => AtviseFile.fromReadResult({}), 'to throw', 'no value');
+    });
+  });
+
+  /** @test {AtviseFile#_getMetadata} */
+  describe('#_getMetadata', function() {
+    tests.forEach(test => {
+      it(test.name, function() {
+        const file = new AtviseFile({ path: test.filePath });
+
+        expect(() => file._getMetadata(), 'not to throw');
+        expect(file._dataType, 'to equal', test.dataType);
+        expect(file._arrayType, 'to equal', test.arrayType);
+        expect(file._typeDefinition, 'to equal', test.typeDefinition);
+      });
+    });
+  });
+
+  function testMetaGetter(name) {
+    beforeEach(() => stub(AtviseFile.prototype, '_getMetadata', () => {}));
+    afterEach(() => AtviseFile.prototype._getMetadata.restore());
+
+    it('should call _getMetadata if not present', function() {
+      const file = new AtviseFile({ path: 'path' });
+      const val = file[name];
+
+      expect(val, 'to be', undefined);
+      expect(AtviseFile.prototype._getMetadata.calledOnce, 'to be', true);
+    });
+  }
+
+  /** @test {AtviseFile#dataType} */
+  describe('#dataType', function() {
+    testMetaGetter('dataType');
+  });
+
+  /** @test {AtviseFile#arrayType} */
+  describe('#arrayType', function() {
+    testMetaGetter('arrayType');
+  });
+
+  /** @test {AtviseFile#typeDefinition} */
+  describe('#typeDefinition', function() {
+    testMetaGetter('typeDefinition');
+  });
+
   /** @test {AtviseFile#isDisplay} */
   describe('#isDisplay', function() {
     it('should return true for AtviseFiles with correct TypeDefinition', function() {
       expect((new AtviseFile({
         path: 'test.display',
-        typeDefinition: new NodeId('VariableTypes.ATVISE.Display'),
+        _typeDefinition: new NodeId('VariableTypes.ATVISE.Display'),
       })).isDisplay, 'to be true');
+    });
+  });
+
+  /** @test {AtivseFile#value} */
+  describe('#value', function() {
+    const val = new Buffer('test');
+
+    before(() => {
+      stub(AtviseFile, 'decodeValue', () => true);
+      stub(AtviseFile, 'encodeValue', () => val);
+    });
+
+    after(() => {
+      AtviseFile.decodeValue.restore();
+      AtviseFile.encodeValue.restore();
+    });
+
+    context('when used as getter', function() {
+      it('should return decodedValue', function() {
+        const file = new AtviseFile({ path: 'path.ext' });
+
+        expect(file.value, 'to equal', true);
+        expect(AtviseFile.decodeValue.calledOnce, 'to be true');
+      });
+    });
+
+    context('when used as setter', function() {
+      it('should set encoded value as contents', function() {
+        const file = new AtviseFile({ path: 'path.ext' });
+        file.value = 13;
+
+        expect(AtviseFile.encodeValue.calledOnce, 'to be true');
+        expect(file.contents, 'to equal', val);
+      });
     });
   });
 });
