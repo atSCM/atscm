adc4b1a016a7d84864469d1cbdad373158410236
Lukas Hechenberger <admin@ls-age.com>
2017-02-22 11:08:03 +0100

    feat(tasks): Add push task

--- src/tasks/push.js
+++ src/tasks/push.js
@@ -1,5 +1,30 @@
+import { src } from 'gulp';
+import ProjectConfig from '../config/ProjectConfig';
+import Transformer, { TransformDirection } from '../lib/transform/Transformer';
+import MappingTransformer from '../transform/Mapping';
+import WriteStream from '../lib/server/WriteStream';
+
 export default function push() {
-  return Promise.reject(new Error('Not implemented: Run push'));
+  let uploaded = 0;
+
+  const mappingStream = new MappingTransformer({ direction: TransformDirection.FromFilesystem });
+  const writeStream = new WriteStream()
+    .on('data', () => uploaded++);
+
+  const printProgress = setInterval(() => {
+    process.stdout.write(`\rUploaded: ${uploaded}`);
+  }, 1000);
+
+  return src('./src/**/*.*')
+    .pipe(mappingStream)
+    .pipe(Transformer.applyTransformers(ProjectConfig.useTransformers,
+      TransformDirection.FromFilesystem))
+    .pipe(writeStream)
+    .on('end', () => {
+      process.stdout.clearLine();
+      process.stdout.write('\r');
+      clearInterval(printProgress);
+    });
 }
 
-push.description = 'Not implemented: Push to the atvise server';
+push.description = 'Push all stored nodes to atvise server';
