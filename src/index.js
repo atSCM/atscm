8cceb686c28d15b404cc9e24653cf2c7e6ba5bd7
Lukas Hechenberger <admin@ls-age.com>
2017-02-27 11:26:20 +0100

    style(ESLint): Fix no-multiple-empty-lines

--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@ export { default as Atviseproject } from './lib/config/Atviseproject';
 export { default as NodeId } from './lib/server/NodeId';
 export { default as Transformer, TransformDirection } from './lib/transform/Transformer';
 export { default as DisplayTransformer } from './transform/DisplayTransformer';
-
-
