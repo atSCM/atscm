d7b48d7fa260d469248837ff61412cbf55dba0bc
Lukas Hechenberger <admin@ls-age.com>
2017-03-01 08:10:10 +0100

    feat(mapping): Support custom variable types
    First step on #12

--- src/transform/Mapping.js
+++ src/transform/Mapping.js
@@ -1,6 +1,10 @@
+import { Buffer } from 'buffer';
+import { readFile } from 'fs';
+import { basename, join } from 'path';
 import Logger from 'gulplog';
 import Transformer from '../lib/transform/Transformer';
 import AtviseFile from '../lib/server/AtviseFile';
+import NodeId from '../lib/server/NodeId';
 
 /**
  * A Transformer that maps {@link ReadStream.ReadResult}s to {@link AtviseFile}s.
@@ -8,7 +12,8 @@ import AtviseFile from '../lib/server/AtviseFile';
 export default class MappingTransformer extends Transformer {
 
   /**
-   * Writes an {@link AtviseFile} for each {@link ReadStream.ReadResult} read.
+   * Writes an {@link AtviseFile} for each {@link ReadStream.ReadResult} read. If a read file has a
+   * non-standard type (definition) an additional `.rc` file is pushed holding this type.
    * @param {ReadStream.ReadResult} readResult The read result to create the file for.
    * @param {String} encoding The encoding used.
    * @param {function(err: ?Error, data: ?AtviseFile)} callback Called with the error that occurred
@@ -16,9 +21,26 @@ export default class MappingTransformer extends Transformer {
    */
   transformFromDB(readResult, encoding, callback) {
     try {
-      callback(null, AtviseFile.fromReadResult(readResult));
+      const file = AtviseFile.fromReadResult(readResult);
+
+      if (file.relative.match(/\.var\./)) {
+        const rc = file.clone();
+
+        rc.extname = '';
+        rc.basename = `.${rc.stem}.rc`;
+
+        rc.contents = Buffer.from(JSON.stringify({
+          typeDefinition: file.typeDefinition,
+        }, null, '  '));
+
+        this.push(rc);
+      }
+
+      callback(null, file);
     } catch (e) {
       Logger.warn(`Unable to map ${readResult.nodeId.toString()}`, e.message);
+      Logger.debug(e);
+
       callback(null);
     }
   }
@@ -34,12 +56,30 @@ export default class MappingTransformer extends Transformer {
     if (file.isDirectory()) {
       callback(null);
     } else {
-      callback(null, new AtviseFile({
+      const atFile = new AtviseFile({
         cwd: file.cwd,
         base: file.base,
         path: file.path,
         contents: file.contents,
-      }));
+      });
+
+      if (file.relative.match(/\.var\./)) {
+        const rcPath = join(file.dirname, `.${basename(file.stem, '.var')}.rc`);
+
+        readFile(rcPath, 'utf8', (err, data) => {
+          try {
+            const rc = JSON.parse(data);
+            atFile._typeDefinition = new NodeId(rc.typeDefinition);
+
+            callback(null, atFile);
+          } catch (e) {
+            Logger.error(`Unable to get runtime configuration for ${file.relative}`);
+            callback(err || e);
+          }
+        });
+      } else {
+        callback(null, atFile);
+      }
     }
   }
 
