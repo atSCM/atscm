2b2a913122f35244440e039ec07ee5ff603cd2ea
Lukas Hechenberger <admin@ls-age.com>
2017-03-06 10:14:12 +0100

    feat(tasks): Add watchForFileChanges

---
+++ src/lib/gulp/watchForServerChanges.js
@@ -0,0 +1,10 @@
+import Logger from 'gulplog';
+import Watcher from '../server/Watcher';
+
+export default function watchForServerChanges(listener) {
+  return () => {
+    return new Watcher()
+      .on('change', data => listener(data))
+      .on('ready', () => Logger.info('Waiting for server changes...'));
+  };
+}
