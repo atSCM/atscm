d3d7cc7dac79b07e917a2acb4da1aa75bd1a0a83
Lukas Hechenberger <admin@ls-age.com>
2017-03-06 10:14:48 +0100

    test: Add tests for watchForServerChanges

---
+++ test/src/lib/gulp/watchForServerChanges.spec.js
@@ -0,0 +1,21 @@
+import expect from 'unexpected';
+import { spy } from 'sinon';
+import watchForServerChanges from '../../../../src/lib/gulp/watchForServerChanges';
+import Watcher from '../../../../src/lib/server/Watcher';
+
+/** @test {watchForServerChanges} */
+describe('watchForServerChanges', function() {
+  it('should return a Watcher', function() {
+    expect(watchForServerChanges(() => {})(), 'to be a', Watcher);
+  });
+
+  it('should forward change events to listener', function() {
+    const listener = spy();
+    const watcher = watchForServerChanges(listener)();
+    const event = {};
+
+    watcher.emit('change', event);
+    expect(listener.calledOnce, 'to be true');
+    expect(listener.lastCall.args[0], 'to equal', event);
+  });
+});
