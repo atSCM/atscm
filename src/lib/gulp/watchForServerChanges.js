4f23a6617da59bf7f17ce1c288973add9129dad7
Lukas Hechenberger <admin@ls-age.com>
2017-03-07 07:34:29 +0100

    style(ESDoc): Add missing function description

--- src/lib/gulp/watchForServerChanges.js
+++ src/lib/gulp/watchForServerChanges.js
@@ -1,6 +1,11 @@
 import Logger from 'gulplog';
 import Watcher from '../server/Watcher';
 
+/**
+ * Returns a fully set up gulp task that watches atvise server nodes for value changes.
+ * @param {function(path: String, root: String, stat: Object)} listener Called when a node changes.
+ * @return {function()} The resulting gulp task.
+ */
 export default function watchForServerChanges(listener) {
   return () => new Watcher()
       .on('change', data => listener(data))
