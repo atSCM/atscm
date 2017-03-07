e4d57401eca0abf03c690956164aad3fc07729b3
Lukas Hechenberger <admin@ls-age.com>
2017-03-07 20:00:03 +0100

    feat(tasks): Use [browser-sync](https://www.browsersync.io) to reload browser on change

--- src/tasks/watch.js
+++ src/tasks/watch.js
@@ -1,5 +1,6 @@
 import { join } from 'path';
-import { parallel, src } from 'gulp';
+import { src } from 'gulp';
+import browserSync from 'browser-sync';
 import Logger from 'gulplog';
 import { obj as createStream } from 'through2';
 import watchForFileChanges from '../lib/gulp/watchForFileChanges';
@@ -7,52 +8,111 @@ import watchForServerChanges from '../lib/gulp/watchForServerChanges';
 import PushStream from '../lib/gulp/PushStream';
 import PullStream from '../lib/gulp/PullStream';
 import AtviseFile from '../lib/server/AtviseFile';
-
-let pulling = false;
-let pushing = false;
-let lastPull = 0;
-let lastPushed = null;
+import ProjectConfig from '../config/ProjectConfig';
 
 /**
  * Watches local files and atvise server nodes to trigger pull/push on change.
  */
-const watch = parallel(
-  watchForFileChanges((path, root, stat) => {
-    if (!pulling && AtviseFile.normalizeMtime(stat.mtime) > lastPull) {
-      pushing = true;
-      Logger.info(path, 'changed');
+export default function watch() {
+  let fileWatcher;
+  let serverWatcher;
+  const browserSyncInstance = browserSync.create();
 
-      const source = src(join(root, path), { base: root });
+  let pulling = false;
+  let pushing = false;
+  let lastPull = 0;
+  let lastPushed = null;
 
-      (new PushStream(source))
-        .on('data', file => (lastPushed = file.nodeId.toString()))
-        .on('end', () => {
-          pushing = false;
-        });
-    }
-  }),
-  watchForServerChanges(readResult => {
-    if (!pushing) {
-      if (readResult.nodeId.toString() !== lastPushed) {
-        pulling = true;
-        Logger.info(readResult.nodeId.toString(), 'changed');
+  function initBrowserSync() {
+    return new Promise((resolve) => {
+      let ready = 0;
 
-        const readStream = createStream();
-        readStream.write(readResult);
-        readStream.end();
+      function initIfReady() {
+        ready++;
 
-        (new PullStream(readStream))
-          .on('end', () => {
-            pulling = false;
-            lastPull = AtviseFile.normalizeMtime(readResult.mtime);
-          });
-      } else {
-        lastPushed = null;
+        if (ready === 2) {
+          browserSyncInstance.init({
+            proxy: {
+              target: `${ProjectConfig.host}:${ProjectConfig.port.http}`,
+              ws: true,
+            },
+          }, () => resolve());
+        }
       }
-    }
-  })
-);
 
-export default watch;
+      fileWatcher.once('ready', () => initIfReady());
+      serverWatcher.once('ready', () => initIfReady());
+    });
+  }
+
+  function rejectOnError(err, resolve, reject) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve();
+    }
+  }
+
+  function startFileWatcher() {
+    return new Promise((resolve, reject) => {
+      fileWatcher = watchForFileChanges((path, root, stat) => {
+        if (!pulling && AtviseFile.normalizeMtime(stat.mtime) > lastPull) {
+          pushing = true;
+          Logger.info(path, 'changed');
+
+          const source = src(join(root, path), { base: root });
+
+          (new PushStream(source))
+            .on('data', file => (lastPushed = file.nodeId.toString()))
+            .on('end', () => {
+              pushing = false;
+              browserSyncInstance.reload();
+              fileWatcher.emit('push');
+            });
+        }
+      })(err => rejectOnError(err, resolve, reject));
+    });
+  }
+
+  function startServerWatcher() {
+    return new Promise((resolve, reject) => {
+      serverWatcher = watchForServerChanges(readResult => {
+        if (!pushing) {
+          if (readResult.nodeId.toString() !== lastPushed) {
+            pulling = true;
+            Logger.info(readResult.nodeId.toString(), 'changed');
+
+            const readStream = createStream();
+            readStream.write(readResult);
+            readStream.end();
+
+            (new PullStream(readStream))
+              .on('end', () => {
+                pulling = false;
+                lastPull = AtviseFile.normalizeMtime(readResult.mtime);
+                browserSyncInstance.reload();
+                fileWatcher.emit('pull');
+              });
+          } else {
+            lastPushed = null;
+          }
+        }
+      })(err => rejectOnError(err, resolve, reject));
+    });
+  }
+
+  const promise = Promise.all([
+    startFileWatcher(),
+    startServerWatcher(),
+  ]);
+
+  initBrowserSync();
+
+  promise.browserSync = browserSyncInstance;
+  promise.fileWatcher = fileWatcher;
+  promise.serverWatcher = serverWatcher;
+
+  return promise;
+}
 
 watch.description = 'Watch local files and atvise server nodes to trigger pull/push on change';
