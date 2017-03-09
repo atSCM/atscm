bcf89f27797697477d09d56067334fcde04ce184
Lukas Hechenberger <admin@ls-age.com>
2017-03-09 12:32:51 +0100

    test(watchForServerChanges): Test log call

--- test/src/lib/gulp/watchForServerChanges.spec.js
+++ test/src/lib/gulp/watchForServerChanges.spec.js
@@ -1,17 +1,18 @@
 import expect from 'unexpected';
 import { spy } from 'sinon';
+import Logger from 'gulplog';
 import watchForServerChanges from '../../../../src/lib/gulp/watchForServerChanges';
 import Watcher from '../../../../src/lib/server/Watcher';
 
 /** @test {watchForServerChanges} */
 describe('watchForServerChanges', function() {
   it('should return a Watcher', function() {
-    expect(watchForServerChanges(() => {})(), 'to be a', Watcher);
+    expect(watchForServerChanges(() => {})(() => {}), 'to be a', Watcher);
   });
 
   it('should forward change events to listener', function() {
     const listener = spy();
-    const watcher = watchForServerChanges(listener)();
+    const watcher = watchForServerChanges(listener)(() => {});
     const event = {};
 
     watcher.emit('change', event);
@@ -26,5 +27,15 @@ describe('watchForServerChanges', function() {
     });
 
     watcher.emit('error', new Error('Test'));
+  });
+
+  it('should log once ready', function() {
+    const watcher = watchForServerChanges(() => {})(() => {});
+    const log = spy();
+
+    Logger.on('info', log);
+    watcher.emit('ready');
+
+    expect(log.calledOnce, 'to be', true);
   });
 });
