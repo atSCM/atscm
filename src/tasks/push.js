c487a6f11b73646849132ecec215b2b4166ab446
Lukas Hechenberger <admin@ls-age.com>
2017-03-13 09:58:12 +0100

    fix(push): Prevent memory leak inside `glob-stream`

--- src/tasks/push.js
+++ src/tasks/push.js
@@ -5,7 +5,7 @@ import PushStream from '../lib/gulp/PushStream';
  * Pushes {@link AtviseFile}s to atvise server.
  */
 export default function push() {
-  return new PushStream(src('./src/**/**/*.*', { buffer: false }));
+  return new PushStream(src('./src/**/*.*'));
 }
 
 push.description = 'Push all stored nodes to atvise server';
