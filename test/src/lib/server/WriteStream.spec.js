2da84a27d1268d9fd3171f00afa47cd8119e93e0
Lukas Hechenberger <admin@ls-age.com>
2017-03-16 15:51:45 +0100

    test(WriteStream): Test status code handling

--- test/src/lib/server/WriteStream.spec.js
+++ test/src/lib/server/WriteStream.spec.js
@@ -1,5 +1,6 @@
 import { stub, spy } from 'sinon';
-import { DataType, VariantArrayType } from 'node-opcua';
+import { DataType, VariantArrayType, StatusCodes, resolveNodeId } from 'node-opcua';
+import Logger from 'gulplog';
 import expect from '../../../expect';
 import WriteStream from '../../../../src/lib/server/WriteStream';
 import AtviseFile from '../../../../src/lib/server/AtviseFile';
@@ -37,7 +38,7 @@ describe('WriteStream', function() {
     it('should call node-opcua~ClientSession#writeSingleNode', function(done) {
       const stream = new WriteStream();
       stream.once('session-open', () => {
-        stub(stream.session, 'writeSingleNode', (node, value, cb) => cb(null, node));
+        stub(stream.session, 'writeSingleNode', (node, value, cb) => cb(null, StatusCodes.Good));
       });
 
       stream.on('data', () => {}); // Unpause readable stream
@@ -56,6 +57,40 @@ describe('WriteStream', function() {
       });
 
       stream.end();
+    });
+
+    it('should warn on access denied status', function() {
+      const stream = new WriteStream();
+
+      stream.once('session-open', () => {
+        stub(stream.session, 'writeSingleNode',
+          (node, value, cb) => cb(null, StatusCodes.BadUserAccessDenied));
+      });
+
+      const nodeId = resolveNodeId('ns=1;s=AGENT.DISPLAYS.Main');
+
+      const warnSpy = spy();
+      Logger.on('warn', warnSpy);
+
+      return expect([{ nodeId }], 'when piped through', stream,
+        'to yield objects satisfying', 'to have length', 1)
+        .then(() => {
+          expect(warnSpy.calledOnce, 'to be true');
+          expect(warnSpy.lastCall.args[0], 'to match', /opened in atvise builder/);
+        });
+    });
+
+    it('should forward invalid status codes', function() {
+      const stream = new WriteStream();
+
+      stream.once('session-open', () => {
+        stub(stream.session, 'writeSingleNode',
+          (node, value, cb) => cb(null, StatusCodes.BadTypeMismatch));
+      });
+
+      return expect([{ nodeId: resolveNodeId('ns=1;s=AGENT.DISPLAYS.Main') }],
+        'when piped through', stream,
+        'to error with', /Error writing node ns=1;s=AGENT\.DISPLAYS\.Main: .+/);
     });
   });
 
@@ -95,5 +130,24 @@ describe('WriteStream', function() {
 
       stream.on('end', done);
     });
+
+    /* it('should forward invalid status codes', function(done) {
+      const stream = new WriteStream();
+      stub(stream, 'writeFile', (file, cb) => cb(null));
+
+      /* stream.on('data', () => {}); // Unpause readable stream
+
+      stream.once('session-open', () => {
+        stream.write(new AtviseFile({ path: 'AGENT/DISPLAYS/Main.display' }));
+        expect(stream.writeFile.calledOnce, 'to be true');
+        stream.end();
+      }); *
+
+      return expect([{ nodeId: resolveNodeId('ns=1;s=AGENT.DISPLAYS.Main') }],
+        'when piped through', stream,
+        'to error with', /Error writing ns=1;s=AGENT\.DISPLAYS\.Main/);
+
+      // stream.on('end', done);
+    }); */
   });
 });
