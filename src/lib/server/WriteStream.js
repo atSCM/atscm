7f6ae1abc3dba8f455a1bbce4d36c0a313ee2fcf
Lukas Hechenberger <admin@ls-age.com>
2017-03-20 10:04:32 +0100

    style(ESDoc): Add method descriptions

--- src/lib/server/WriteStream.js
+++ src/lib/server/WriteStream.js
@@ -2,13 +2,27 @@ import Logger from 'gulplog';
 import { StatusCodes } from 'node-opcua';
 import QueueStream from './QueueStream';
 
+/**
+ * A stream that writes all read {@link AtviseFile}s to their corresponding nodes on atvise server.
+ */
 export default class WriteStream extends QueueStream {
 
+  /**
+   * The error message to use when writing a file fails.
+   * @param {AtviseFile} file The file being processed.
+   * @return {String} The error message to use.
+   */
   processErrorMessage(file) {
     return `Error writing ${file.nodeId.toString()}`;
   }
 
-  processChunk(file, callback) {
+  /**
+   * Writes an {@link AtviseFile} to it's corresponding node on atvise server.
+   * @param {AtviseFile} file The file to write.
+   * @param {function(err: Error, statusCode: node-opcua~StatusCodes, onSuccess: function)}
+   * handleErrors The error handler to call. See {@link QueueStream#processChunk} for details.
+   */
+  processChunk(file, handleErrors) {
     try {
       this.session.writeSingleNode(file.nodeId.toString(), {
         dataType: file.dataType,
@@ -19,16 +33,16 @@ export default class WriteStream extends QueueStream {
           Logger.warn(`Error writing node ${
             file.nodeId.toString()
           }: Make sure it is not opened in atvise builder`);
-          callback(err, StatusCodes.Good, done => done());
+          handleErrors(err, StatusCodes.Good, done => done());
         } else {
-          callback(err, statusCode, done => {
+          handleErrors(err, statusCode, done => {
             this.push(file);
             done();
           });
         }
       });
     } catch (e) {
-      callback(e);
+      handleErrors(e);
     }
   }
 
