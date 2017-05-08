ae098df4480546aba3b521e74b93a876fc47bb90
Lukas Hechenberger <admin@ls-age.com>
2017-05-08 07:36:59 +0200

    ci(circleci): Build on demo.atvise.com

--- test/fixtures/Atviseproject.babel.js
+++ test/fixtures/Atviseproject.babel.js
@@ -3,18 +3,21 @@ import { Atviseproject } from '../../';
 export default class TestProject extends Atviseproject {
 
   static get host() {
-    return '185.67.228.82';
+    return '185.67.228.66';
   }
 
   static get port() {
     return {
-      opc: 4845,
-      http: 80,
+      opc: 4888,
+      http: 8888,
     };
   }
 
   static get login() {
-    return false;
+    return {
+      username: process.env.ATVISE_USERNAME,
+      password: process.env.ATVISE_PASSWORD,
+    };
   }
 
 }
