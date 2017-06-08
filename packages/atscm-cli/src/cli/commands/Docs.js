0e92be7091d1750c650de032b4be960cc613906f
Lukas Hechenberger <LukasHechenberger@users.noreply.github.com>
2017-06-08 11:39:10 +0200

    feat(docs): Open hosted API documentation with `--remote` option passed (#18)
    Fixes #13

--- packages/atscm-cli/src/cli/commands/Docs.js
+++ packages/atscm-cli/src/cli/commands/Docs.js
@@ -1,4 +1,5 @@
 import { join } from 'path';
+import { resolve } from 'url';
 import open from 'open';
 import Command from '../../lib/cli/Command';
 import CliOptions from '../Options';
@@ -10,6 +11,14 @@ import Logger from '../../lib/util/Logger';
 export default class DocsCommand extends Command {
 
   /**
+   * Base URL of the hosted API documentation.
+   * @type {string}
+   */
+  static get RemoteDocsBase() {
+    return 'https://doc.esdoc.org/github.com/atSCM/';
+  }
+
+  /**
    * Creates a new {@link DocsCommand} with the specified name and description.
    * @param {String} name The command's name.
    * @param {String} description The command's description.
@@ -19,17 +28,18 @@ export default class DocsCommand extends Command {
       options: {
         cli: CliOptions.cli,
         browser: CliOptions.browser,
+        remote: CliOptions.remote,
       },
       maxArguments: 0,
     });
   }
 
   /**
-   * Returns the path to the api docs to open.
+   * Returns the path to the local api docs.
    * @param {AtSCMCli} cli The current Cli instance.
-   * @return {String} The path to the api docs to opten.
+   * @return {string} The path to the local api docs.
    */
-  pathToOpen(cli) {
+  localDocsPath(cli) {
     return join(
       cli.options.cli ?
         join(__dirname, '../../../') :
@@ -39,14 +49,54 @@ export default class DocsCommand extends Command {
   }
 
   /**
+   * Returns the URL of the remote api docs.
+   * @param {AtSCMCli} cli The current Cli instance.
+   * @return {string} The URL of the remote api docs.
+   */
+  remoteDocsUrl(cli) {
+    return resolve(this.constructor.RemoteDocsBase, cli.options.cli ?
+      'atscm-cli' :
+      'atscm');
+  }
+
+  /**
+   * Returns the path or url to open. This is resolved in the following way:
+   *  1. If `--remote` is passed, always return the URL of the hosted docs of atscm-cli or atscm
+   *     based on the `--cli` option passed.
+   *  2. If `--cli` is passed, always return the path to the local atscm-cli docs.
+   *  3. Otherwise, check if a local module was found:
+   *     - If *true* return the local module docs path,
+   *     - else return the URL of the hosted atscm docs.
+   * @param {AtSCMCli} cli The calling Cli instance.
+   * @return {{address: string, isPath: boolean}} The resolved address and a flag indicating if the
+   * address describes a file path.
+   */
+  addressToOpen(cli) {
+    if (cli.options.remote !== true && (cli.options.cli || cli.environment.modulePath)) {
+      return {
+        address: this.localDocsPath(cli),
+        isPath: true,
+      };
+    }
+
+    return {
+      address: this.remoteDocsUrl(cli),
+      isPath: false,
+    };
+  }
+
+  /**
    * Opens the requested docs in the requested browser.
    * @param {AtSCMCli} cli The current Cli instance.
+   * @return {Promise} Resolved after the os-specific open command was started.
    */
   run(cli) {
-    const docsPath = this.pathToOpen(cli);
-    Logger.debug('Opening', Logger.format.path(docsPath));
-
-    open(docsPath, cli.options.browser);
+    return (!cli.options.cli && !cli.environment ? cli.getEnvironment() : Promise.resolve())
+      .then(() => this.addressToOpen(cli))
+      .then(({ address, isPath }) => {
+        Logger.debug('Opening', isPath ? Logger.format.path(address) : address);
+        open(address, cli.options.browser);
+      });
   }
 
   /**
@@ -55,7 +105,7 @@ export default class DocsCommand extends Command {
    * @return {Boolean} `false` if the `--cli` option is used.
    */
   requiresEnvironment(cli) {
-    return !cli.options.cli;
+    return cli.options.remote === false && !cli.options.cli;
   }
 
 }
