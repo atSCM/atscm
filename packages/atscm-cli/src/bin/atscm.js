310e68d167185bd30c6d2438724f1327ba0f5a7f
Lukas Hechenberger <LukasHechenberger@users.noreply.github.com>
2017-06-09 09:30:13 +0200

    feat: Automatically check for package updates (#21)
    Fixes #19

--- packages/atscm-cli/src/bin/atscm.js
+++ packages/atscm-cli/src/bin/atscm.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+import updateNotifier from 'update-notifier';
+import pkg from '../../package.json';
 import AtscmCli from '../AtSCMCli';
+
+updateNotifier({ pkg }).notify();
 
 (new AtscmCli(process.argv.slice(2))).launch();
