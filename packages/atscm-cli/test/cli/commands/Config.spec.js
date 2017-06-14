819bb0d5a0ab731c6e5dc187766833fe6920605d
Lukas Hechenberger <LukasHechenberger@users.noreply.github.com>
2017-06-14 08:14:11 +0200

    feat: Pass options by setting env vars named `ATSCM_{options}={value}` (#23)

--- packages/atscm-cli/test/cli/commands/Config.spec.js
+++ packages/atscm-cli/test/cli/commands/Config.spec.js
@@ -1,3 +1,4 @@
+import { join } from 'path';
 import expect from 'unexpected';
 import { stub } from 'sinon';
 
@@ -10,23 +11,64 @@ describe('ConfigCommand', function() {
   describe('#run', function() {
     beforeEach(function() {
       stub(Logger, 'info');
+      stub(Logger, 'warn');
     });
 
     afterEach(function() {
       Logger.info.restore();
+      Logger.warn.restore();
     });
 
     const command = new ConfigCommand('config', 'Print configuration.');
 
-    it('should print the current configuration', function() {
-      command.run({
-        environment: {
-          configPath: __filename,
-        },
+    context('when overrides are available (atscm >= v0.4)', function() {
+      it('should print local module\'s ProjectConfig class', function() {
+        command.run({
+          options: {
+            project: {},
+          },
+          environment: {
+            modulePath: join(__dirname, '../../fixtures/node_modules/atscm/fake/v0-4.js'),
+            configPath: join(__dirname, '../../fixtures/Atviseproject.js'),
+          },
+        });
+
+        expect(Logger.info.calledOnce, 'to be', true);
+        expect(Logger.info.lastCall.args[0], 'to match', new RegExp('^Configuration at'));
+      });
+    });
+
+    context('when overrides are not available (atscm < v0.4)', function() {
+      it('should print local module\'s Atviseproject class', function() {
+        command.run({
+          options: {
+            project: {},
+          },
+          environment: {
+            modulePath: join(__dirname, '../../fixtures/node_modules/atscm/fake/v0-3.js'),
+            configPath: join(__dirname, '../../fixtures/Atviseproject.js'),
+          },
+        });
+
+        expect(Logger.info.callCount, 'to equal', 2);
+        expect(Logger.info.firstCall.args[0], 'to match', new RegExp('^Configuration at'));
       });
 
-      expect(Logger.info.calledOnce, 'to be', true);
-      expect(Logger.info.lastCall.args[0], 'to match', new RegExp('^Configuration at'));
+      it('should warn that overrides are not available', function() {
+        command.run({
+          options: {
+            project: {},
+          },
+          environment: {
+            modulePath: join(__dirname, '../../fixtures/node_modules/atscm/fake/v0-3.js'),
+            configPath: join(__dirname, '../../fixtures/Atviseproject.js'),
+          },
+        });
+
+        expect(Logger.warn.calledOnce, 'to be', true);
+        expect(Logger.warn.lastCall.args[0], 'to contain',
+          'Overriding runtime configuration requires atscm version >= 0.4');
+      });
     });
   });
 });
