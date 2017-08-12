ab6633fbaa9c767fd979219db284e6f1348d8b2d
Kevin Horvath <kevin.horvath@certec.at>
2017-08-12 15:17:34 +0200

    feat: Map all typedefinitions to file system
    Introduces TypeDefStream, which processes UA reference descriptions
    and creates type definition objects, which are processed in the following
    streams and transformers. This commit also includes changes to make it
    possible to process the new type definition objects.

--- src/lib/server/NodeStream.js
+++ src/lib/server/NodeStream.js
@@ -1,7 +1,29 @@
-import { browse_service as BrowseService, NodeClass } from 'node-opcua';
+import { browse_service as BrowseService, NodeClass, ReferenceTypeIds} from 'node-opcua';
 import QueueStream from './QueueStream';
 import NodeId from './NodeId';
 import Project from '../../config/ProjectConfig';
+import Logger from 'gulplog';
+
+/**
+ * Object that contains the corresponding derived types for object and
+ * variable types
+ * @type {node-opcua~NodeClass{}}
+ */
+const NodeClassForTypeDefinition = {
+  [NodeClass.VariableType]: NodeClass.Variable,
+  [NodeClass.ObjectType] : NodeClass.Object,
+};
+
+/**
+ * List of valid reference types
+ * @type {node-opcua~ReferenceTypeId{}}
+ */
+const ValidReferenceTypes = [
+  ReferenceTypeIds.HasComponent,
+  ReferenceTypeIds.HasProperty,
+  ReferenceTypeIds.HasTypeDefinition,
+  ReferenceTypeIds.HasSubtype
+];
 
 /**
  * A stream that browses the nodes specified and (if *recursive* option is set) it's child nodes.
@@ -52,9 +74,10 @@ export default class NodeStream extends QueueStream {
      * A regular expression matching all node ids specified in {@link NodeStream#ignoreNodes}
      * @type {RegExp}
      */
-    this.ignoredRegExp = new RegExp(`^(${ignoreNodes.map(n => n.toString()).join('|')})`);
+    this.ignoredRegExp = new RegExp(`^(${ignoreNodes.map(n => n.value.toString()).join('|')})`);
 
     // Write nodes to read
+    // Append nodes to queue
     nodesToBrowse.forEach(nodeId => this.write(nodeId));
 
     // End once drained
@@ -73,12 +96,98 @@ export default class NodeStream extends QueueStream {
   }
 
   /**
+   * Changes reference properties if the reference is a type definition
+   * for proper handling in type definition stream
+   * @param{node-opcua~ReferenceDescription} reference description which gets checked
+   * @param{node-opcua~NodeId} nodeId The browsed nodeId
+   */
+  convertRefToTypeDef(ref, nodeId) {
+    ref.isTypeDef = true;
+    ref.$nodeClass = NodeClassForTypeDefinition[ref.$nodeClass];
+    ref.typeDefinition = ref.nodeId;
+    ref.nodeId = nodeId;
+  }
+
+  /**
+   * Checks if the given reference matches the defined browse Filters
+   * @param{node-opcua~ReferenceDescription} ref The reference description to check
+   * @param{node-opcua~NodeId} nodeId The browsed nodeId
+   * @return {Bool} reference matches browse filters(=true) or not(=false)
+   */
+  matchesFilter(ref, nodeId) {
+    let refNodeId = ref.nodeId.toString();
+    let test = refNodeId.split(nodeId.value);
+    let test2 = NodeClassForTypeDefinition.hasOwnProperty(ref.$nodeClass);
+
+
+    // Only let valid reference types and object and variable types pass
+    return ValidReferenceTypes.indexOf(ref.referenceTypeId.value) > -1 &&
+      (NodeClassForTypeDefinition.hasOwnProperty(ref.$nodeClass) ||
+        ref.nodeId.toString().split(nodeId.value).length > 1) && !this.isIgnored(ref)
+  }
+
+  /**
+   * Checks if the given reference matches the defined browse Filters
+   * @param{node-opcua~ReferenceDescription} ref The reference description to check
+   * @return {Bool} reference is a type definition(=true) or not(=false)
+   */
+  isTypeDefinition(ref) {
+    return ref.referenceTypeId.value === ReferenceTypeIds.HasTypeDefinition;
+  }
+
+  /**
+   * Checks if the given reference should be pushed to NodeStreamOutput or not
+   * @param{node-opcua~ReferenceDescription} ref The reference description to check
+   * @return {Bool} reference sould be pushed(=true) or not(=false)
+   */
+  shouldBePushed(ref) {
+    return ref.$nodeClass.value === NodeClass.Object.value ||
+      ref.referenceTypeId.value === ReferenceTypeIds.HasSubtype;
+  }
+
+  /**
+   * Checks if the given rference description is ignored
+   * @param{node-opcua~ReferenceDescription} ref The reference description to check
+   * @return {Bool} Result of the check
+   */
+  isIgnored(ref) {
+    let refNodeId = ref.nodeId.value.toString();
+
+    if (refNodeId.match(this.ignoredRegExp)) {
+      Logger.debug(`Ignored node: ${refNodeId}`);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Pushes the given reference to output of NodeStream
+   * @param{node-opcua~ReferenceDescription} ref The reference description to push
+   * @param{node-opcua~NodeId} nodeId The browsed nodeId
+   * @return {Bool} Type definition got pushed(=true) or variable type(=false)
+   */
+  pushReference(ref, nodeId) {
+    let isTypeDef = this.isTypeDefinition(ref);
+
+    if (isTypeDef) {
+      this.convertRefToTypeDef(ref, nodeId);
+    }
+
+    // "cast" reference nodeId to NodeId
+    Object.setPrototypeOf(ref.nodeId, NodeId.prototype);
+    this.push(ref); // write ref to Nodestream output (stdout)
+
+    return isTypeDef;
+  }
+
+  /**
    * Returns a {ReadStream.ReadResult} for the given reference description.
    * @param {NodeId} nodeId The node id to browse.
    * @param {function(err: Error, statusCode: node-opcua~StatusCodes, onSuccess: function)}
    * handleErrors The error handler to call. See {@link QueueStream#processChunk} for details.
    */
   processChunk(nodeId, handleErrors) {
+    // browses forward
     this.session.browse({
       nodeId,
       browseDirection: BrowseService.BrowseDirection.Forward,
@@ -91,23 +200,18 @@ export default class NodeStream extends QueueStream {
         handleErrors(err, results && results.length > 0 ? results[0].statusCode : null, done => {
           Promise.all(
             results[0].references
-            // Remove parent nodes
-              .filter(ref => ref.nodeId.value.toString().split(nodeId.value).length > 1)
-              // Ignore specified nodes
-              // TODO: Print ignored nodes (debug level)
-              .filter(ref => !(ref.nodeId.toString().match(this.ignoredRegExp)))
-              // Push variable nodes, recurse
+              // Remove parent nodes and ignore specified nodes
+              .filter(ref => this.matchesFilter(ref, nodeId))
+              // Push variable and object nodes, recurse
               .map(ref => {
-                // Push all variable ids
-                if (ref.nodeClass.value === NodeClass.Variable.value) {
-                  // "Cast" ref.nodeId to NodeId
-                  Object.setPrototypeOf(ref.nodeId, NodeId.prototype);
+                // only push non object components to NodeStream output
+                let browseRef = this.shouldBePushed(ref) ? true :
+                  !this.pushReference(ref, nodeId);
 
-                  this.push(ref);
-                }
-
-                if (this.recursive) {
+                // Only browse variable types and objects recursively
+                if (this.recursive && browseRef) {
                   return new Promise((resolve) => {
+                    // add ref nodeid as chunk to NodeStream queue (stdin) --> recursive browsing of nodes
                     this.write(ref.nodeId, null, resolve);
                   });
                 }
@@ -115,7 +219,8 @@ export default class NodeStream extends QueueStream {
                 return Promise.resolve();
               })
           )
-            .then(done);
+            .then(done); // when all references (per browsed nodeId) have been processed, done callback is fired
+                         // then the read stream can process the given data chunk by chunk
         });
       }
     });
