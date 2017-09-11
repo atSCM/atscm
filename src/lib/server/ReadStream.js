f15cfa292b53d93ace960bb9eb72b39df2631418
Kevin Horvath <kevin.horvath@certec.at>
2017-09-11 16:28:21 +0200

    feat: Map all neccessary reference types to filesystem
    Applies several changes to make it possible to map all
    kinds of reference types to the file system. A node configuration file type
    is introduced that contains information about the typedefinition of a atvise
    node and all needed references to make it possible to instantiate new
    nodes in the push stream

--- src/lib/server/ReadStream.js
+++ src/lib/server/ReadStream.js
@@ -1,5 +1,6 @@
 import QueueStream from './QueueStream';
-import { StatusCodes, NodeClass} from 'node-opcua';
+import {StatusCodes, NodeClass} from 'node-opcua';
+import ReadStreamResult from './ReadStreamResult';
 
 /**
  * A stream that reads atvise server nodes for the {@link node-opcua~ReferenceDescription}s passed.
@@ -23,42 +24,24 @@ export default class ReadStream extends QueueStream {
    * @param {function(err: Error, statusCode: node-opcua~StatusCodes, onSuccess: function)}
    * handleErrors The error handler to call. See {@link QueueStream#processChunk} for details.
    */
-  processChunk(typeDefResult, handleErrors) {
-    const nodeId = typeDefResult.nodeId;
+  processChunk(nodeConfigStreamResult, handleErrors) {
+    const nodeId = nodeConfigStreamResult.sourceNodeId;
 
-    if (typeDefResult.isTypeDef) {
-        this.push(typeDefResult);
-        this._processNextChunk(typeDefResult);
+    if (nodeConfigStreamResult.isNodeConfig) {
+      this.push(new ReadStreamResult(nodeConfigStreamResult));
+      this._processNextChunk(nodeConfigStreamResult);
     } else {
       this.session.read([{nodeId}], (err, nodesToRead, results) => {
         if(! err && (! results || results.length === 0)) {
           handleErrors(new Error('No results'));
         } else {
           handleErrors(err, results && results.length > 0 ? results[0].statusCode : null, done => {
-            let readValue = results[0].value;
-            this.push({
-              nodeId,
-              browseName: typeDefResult.browseName.name,
-              value: readValue.value,
-              dataType: readValue.$dataType,
-              arrayType: readValue.$arrayType,
-              typeDefinition: typeDefResult.typeDefinition,
-              mtime: results[0].sourceTimestamp,
-            });
+            this.push(new ReadStreamResult(nodeConfigStreamResult, results[0]));
             done();
-        });
-      }
+          });
+        }
       });
     }
   }
 }
 
-/**
- * @typedef {Object} ReadStream.ReadResult
- * @property {NodeId} nodeId The read node's id.
- * @property {?node-opcua~value} value The read value.
- * @property {?node-opcua~dataType} dataType The data type of the read value.
- * @property {?node-opcua~arrayType} arrayType The array type of the read value.
- * @property {Object} referenceDescription Additional info on the read node.
- * @property {Date} mtime The timestamp the node's value last changed.
- */
