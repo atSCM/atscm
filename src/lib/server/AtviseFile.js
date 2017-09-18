e9c44e0c91d0e4c951e72228b4f411a79bd4f5b1
Kevin Horvath <kevin.horvath@certec.at>
2017-09-18 17:06:37 +0200

    fix: add int 64 variable support
    Changes encoding and decoding of 64 bit int variables, because node-opcua
    creates two 32 bit int variables out of one int 64 variable. This happens because the
    javascript number has only 53 bit integer range

--- src/lib/server/AtviseFile.js
+++ src/lib/server/AtviseFile.js
@@ -56,11 +56,13 @@ const Decoder = {
   [DataType.NodeId]: stringValue => resolveNodeId(stringValue),
   [DataType.DateTime]: stringValue => new Date(Number.parseInt(stringValue, 10)),
   [DataType.UInt64]: stringValue => JSON.parse(stringValue),
+  [DataType.Int64]: stringValue => JSON.parse(stringValue)
 };
 
 const Encoder = {
   [DataType.DateTime]: date => date.getTime().toString(),
   [DataType.UInt64]: uInt32Array => JSON.stringify(uInt32Array),
+  [DataType.Int64]: int32Array => JSON.stringify(int32Array),
   [DataType.ByteString]: binaryArray => new Buffer(binaryArray, 'binary')
 };
 
