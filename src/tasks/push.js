fbc03888fe83428adfe74e6f8738cc2e32dfef99
Kevin Horvath <kevin.horvath@certec.at>
2017-09-18 17:16:23 +0200

    feat: combine gulp src streams
    Combines src streams for the given nodeids in project
    config, so a correct push order can be maintained. E.g.
    objecttypes need to be created before instances.

--- src/tasks/push.js
+++ src/tasks/push.js
@@ -1,11 +1,16 @@
 import { src } from 'gulp';
 import PushStream from '../lib/gulp/PushStream';
+import CombinedStream from 'combined-stream';
+import ProjectConfig from '../config/ProjectConfig';
 
 /**
  * Pushes {@link AtviseFile}s to atvise server.
  */
 export default function push() {
-  return new PushStream(src('./src/**/*.*'));
+  const combinedSrcStream = CombinedStream.create();
+
+  ProjectConfig.nodes.map(nodeId => combinedSrcStream.append(src(`./src/${nodeId.filePath}/**/*.*`)));
+  return new PushStream(combinedSrcStream);
 }
 
 push.description = 'Push all stored nodes to atvise server';
