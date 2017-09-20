5e33955acfa96f49e58d7cd0b428bb12fcb426cb
Kevin Horvath <kevin.horvath@certec.at>
2017-09-20 15:01:00 +0200

    feat: add nodeid value to reference type files
    Add the numeric node id for mapped referenc types so
    the node id of the given type can be used in the push task

--- src/lib/server/MappingItem.js
+++ src/lib/server/MappingItem.js
@@ -207,9 +207,12 @@ export default class MappingItem {
     }
 
     if (refConfig.hasOwnProperty(referenceName)) {
-      refConfig[referenceName].push(this.createRefConfigObj(ref, isObjOrVarTypeRef));
+      refConfig[referenceName].items.push(this.createRefConfigObj(ref, isObjOrVarTypeRef));
     } else {
-      refConfig[referenceName] = [this.createRefConfigObj(ref, isObjOrVarTypeRef)];
+      refConfig[referenceName] = {
+        referenceIdValue: ReferenceTypeIds[referenceName],
+        items: [this.createRefConfigObj(ref, isObjOrVarTypeRef)]
+      };
     }
   }
 
@@ -224,7 +227,7 @@ export default class MappingItem {
 
     return {
       refNodeId: refNodeId.toString(),
-      nodeClass: isObjOrVarTypeRef ? ref.nodeClass.key: InstanceNodeClasses[ref.nodeClass],
+      nodeClass: isObjOrVarTypeRef ? ref.$nodeClass.key : InstanceNodeClasses[ref.$nodeClass],
       typeDefinition: ref.typeDefinition.toString()
     }
   }
