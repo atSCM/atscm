772bc156df2dbf4c3b7ea43b1ea5b6780158073d
Kevin Horvath <kevin.horvath@certec.at>
2017-10-04 17:08:21 +0200

    feat: change xml parser to maintain element order
    Replaces xml2js parser with xamel xml parser to maintain order
    of scripts and displays.
    This commit updates the package.json file

--- src/lib/transform/XMLTransformer.js
+++ src/lib/transform/XMLTransformer.js
@@ -1,19 +1,24 @@
 import { EOL } from 'os';
-import { parseString as parseXML, Builder as XMLBuilder } from 'xml2js';
 import { TransformDirection } from './Transformer';
 import SplittingTransformer from './SplittingTransformer';
+import {parse as xmlStringToObj, serialize as objToXmlString} from 'xamel';
+import * as xmlLib from 'xamel/lib/xml';
 
 /**
- * A special token used to encode CData section beginnings.
+ * Definition for default xml header
  * @type {String}
  */
-const START_CDATA = 'STARTCDATA';
+const XmlHeader = `<?xml version='1.0' encoding='UTF-8' standalone='no'?>\n`;
 
 /**
- * A special token used to encode CData section endings.
- * @type {String}
+ * List of XML Tags needed for relative node addresses
+ * @type {String[]}
  */
-const END_CDATA = 'ENDCDATA';
+const RelativePathElements = [
+  'Elements',
+  'RelativePathElement',
+  'TargetName'
+];
 
 /**
  * A transformer used to transform XML documents.
@@ -26,43 +31,74 @@ export default class XMLTransformer extends SplittingTransformer {
    */
   constructor(options) {
     super(options);
-
-    /**
-     * The builder to use with direction {@link TransformDirection.FromDB}.
-     * @type {xml2js~Builder}
-     */
-    this._fromDBBuilder = new XMLBuilder({
-      cdata: false,
-      newline: EOL,
-    });
-
-    /**
-     * The builder to use with direction {@link TransformDirection.FromFilesystem}.
-     * @type {xml2js~Builder}
-     */
-    this._fromFilesystemBuilder = new XMLBuilder({
-      renderOpts: {
-        pretty: true,
-        indent: ' ',
-        newline: '\r\n',
-      },
-      xmldec: {
-        version: '1.0',
-        encoding: 'UTF-8',
-        standalone: false,
-      },
-      cdata: true,
-    });
   }
 
   /**
-   * Returns the XML builder instance to use base on the current {@link Transformer#direction}.
-   * @type {xml2js~Builder}
+   * Creates a new {xamel~NodeSet}
+   * @param {xamel~Tag[]} scriptCode The script code to store
    */
-  get builder() {
-    return this.direction === TransformDirection.FromDB ?
-      this._fromDBBuilder :
-      this._fromFilesystemBuilder;
+  createNodeSet(nodes) {
+    return new xmlLib.NodeSet(nodes);
+  }
+
+  /**
+   * Creates a new {xamel~Tag}
+   * @param {String} tagName The xml tag name
+   * @param {Object} attributes Object containing xml attributes
+   * @param {xamel~Tag} parentTag The parent tag the created tag belongs to
+   * @param {*} initialChildNode The tags child item
+   */
+  createTag(tagName, attributes, parentTag, initialChildNode) {
+    const sortedAttr = Object.keys(attributes).sort().forEach(key => attributes[key])
+    const tag = new xmlLib.Tag(tagName, attributes, parentTag);
+
+    if (initialChildNode) {
+      if (initialChildNode instanceof xmlLib.Tag) {
+        initialChildNode.parent = tag;
+      }
+      tag.append(initialChildNode);
+    }
+
+    return tag;
+  }
+
+
+  /**
+   * Creates a new {xamel~CData}
+   * @param {String} scriptCode The script code to store
+   */
+  createCData(scriptCode) {
+    return new xmlLib.CData(scriptCode);
+  }
+
+  /**
+   * Creates a new <RelativePath> tag
+   * @param {Object} config The relative path elements configuration object
+   * @param {xamel~Tag} parentTag The parent tag the relative path tag belongs to
+   */
+  createRelPathTag(configObj, parentTag) {
+    const relPathTag = this.createTag('RelativePath', {}, parentTag);
+
+    if (configObj) {
+      let currParentTag = relPathTag;
+
+      // Build relative path element structure
+      RelativePathElements.forEach(tagName => {
+        let tag = this.createTag(tagName, {}, currParentTag);
+        currParentTag.append(tag);
+        currParentTag = tag;
+      });
+
+      // Append relative path information
+      currParentTag
+        .append(this.createTag('NamespaceIndex', {}, currParentTag, configObj.nameSpaceIndex))
+        .append(this.createTag('Name', {}, currParentTag, configObj.nodePath));
+
+    } else {
+      relPathTag.append(this.createTag('Elements', {}, relPathTag));
+    }
+
+    return relPathTag;
   }
 
   /**
@@ -72,43 +108,30 @@ export default class XMLTransformer extends SplittingTransformer {
    * parse error that occurred.
    */
   decodeContents(file, callback) {
-    parseXML(file.contents, callback);
+    xmlStringToObj(file.contents.toString(),
+     {
+       cdata: true,
+       strictEntities: true,
+       normalize: true
+     },
+     callback);
   }
 
   /**
    * Builds an XML string from an object.
-   * @param {Object} object The object to encode.
+   * @param {xml2js~Builder} xmlObj The object to encode.
    * @param {function(err: ?Error, result: ?String)} callback Called with the resulting string or
    * the error that occurred while building.
    */
-  encodeContents(object, callback) {
+  encodeContents(xmlObj, callback) {
     try {
-      callback(null,
-        this.builder.buildObject(object)
-          .replace(new RegExp(`(<!\\[CDATA\\[)?${START_CDATA}`), '<![CDATA[')
-          .replace(new RegExp(`${END_CDATA}(\\]\\]>)?`), ']]>')
-      );
+      callback(null, `${XmlHeader}${objToXmlString(xmlObj, 
+        {
+          pretty: true,
+          header: false
+        })}`);
     } catch (e) {
       callback(e);
     }
   }
-
-  /**
-   * Helper function: Returns `true` if the given tag exists and is not empty.
-   * @param {Object} tag A tag in a parsed xml document.
-   * @return {Boolean} `true` if the given tag exists and is not empty.
-   */
-  tagNotEmpty(tag) {
-    return Boolean(tag && tag.length > 0);
-  }
-
-  /**
-   * Forces `string`, when assigned as textContent to a node, to be wrapped in a CDATA-section.
-   * @param {String} string The string to force a CDATA-section for.
-   * @return {String} The string to assign as textContent to a node.
-   */
-  static forceCData(string) {
-    return `${START_CDATA}${string}${END_CDATA}`;
-  }
-
 }
