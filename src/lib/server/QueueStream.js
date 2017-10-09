a66eeadc55eae19ea3814c9fbbe2e86fbac50f15
Kevin Horvath <kevin.horvath@certec.at>
2017-10-09 16:45:38 +0200

    refactor: turn multiline error message to single line message

--- src/lib/server/QueueStream.js
+++ src/lib/server/QueueStream.js
@@ -167,8 +167,7 @@ export default class QueueStream extends Stream {
       if (err) {
         this.emit('error', new Error(`${this.processErrorMessage(chunk)}: ${err.message}`));
       } else if (statusCode !== StatusCodes.Good) {
-        this.emit('error',
-          new Error(`${this.processErrorMessage(chunk)}: ${statusCode.description}`));
+        this.emit('error', new Error(`${this.processErrorMessage(chunk)}: ${statusCode.description}`));
       } else {
         onSuccess(() => {
           this._processing--;
