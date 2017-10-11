09b76aeeb3855e3c7d7ce8c53417bcfd77203f15
Kevin Horvath <kevin.horvath@certec.at>
2017-10-11 18:21:38 +0200

    feat: introduce diff task

---
+++ src/lib/diff/DiffItemStream.js
@@ -0,0 +1,66 @@
+import QueueStream from '../stream/QueueStream';
+import Logger from 'gulplog';
+import DiffItem from './DiffItem';
+import { StatusCodes } from 'node-opcua';
+
+/**
+ * A stream checks if the given {@link NodeId}s exist on the atvise server or on the filesystem, depending on the
+ * stream direction
+ */
+export default class DiffItemStream extends QueueStream {
+
+  /**
+   * Creates a new DiffItemStream.
+   * @param {Object} The options to use.
+   */
+  constructor(options = {}) {
+    super();
+
+    /**
+     * The diff item cache
+     * @type {Map<vinyl~path, DiffItem>}
+     */
+    this.itemsCache = {};
+
+  }
+
+  /**
+   * Returns an error message specifically for the given mapping item.
+   * @param {DiffFile} file The diff file to create the error message for.
+   * @return {String} The specific error message.
+   */
+  processErrorMessage(file) {
+    return `DiffStream#processErrorMessage: Error processing item ${file.toString()}`;
+  }
+
+
+  /**
+   * Combines the given DiffFiles to DiffItems.
+   * @param {DiffFile} file The file to process
+   * @param {function(err: Error, statusCode: node-opcua~StatusCodes, onSuccess: function)}
+   * handleErrors The error handler to call. See {@link QueueStream#processChunk} for details.
+   */
+
+  processChunk(file, handleErrors) {
+    handleErrors(null, StatusCodes.Good, done => {
+      const path = file.path;
+      const itemsCache = this.itemsCache;
+
+      if (itemsCache.hasOwnProperty(path)) {
+        const diffItem = itemsCache[path];
+
+        diffItem.addFile(file);
+
+        if (diffItem.isComplete) {
+          this.push(diffItem);
+          delete itemsCache[path];
+        }
+      } else {
+        itemsCache[path] = new DiffItem(file);
+      }
+
+      done();
+    });
+  }
+}
+
