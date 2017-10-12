2fe64bcf6979ee93dd40ac51978a976f12abe363
Kevin Horvath <kevin.horvath@certec.at>
2017-10-12 16:07:36 +0200

    feat: introduce deleteServer and deleteFs task

---
+++ src/tasks/deleteServer.js
@@ -0,0 +1,15 @@
+import DeleteServerStream from '../lib/gulp/DeleteServerStream';
+import ProjectConfig from '../config/ProjectConfig';
+
+/**
+ * Deletes listed atvise server nodes.
+ */
+export default function deleteServer (callback) {
+
+  return new DeleteServerStream({
+    deleteFileName: ProjectConfig.DeleteFileNames.server
+  })
+    .on('finish', callback)
+}
+
+deleteServer.description = 'Deletes listed nodes from atvise server';
