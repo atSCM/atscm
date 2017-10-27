d41df50e8269abe8d3c6572d5d270d255b39ec97
CircleCI <circleci@circleci>
2017-10-27 07:53:59 +0000

    chore(release): Add v0.4.0-beta.4 release files [ci skip]

---
+++ out/lib/server/Watcher.js
@@ -0,0 +1,211 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SubscribeStream = undefined;
+
+var _events = require('events');
+
+var _events2 = _interopRequireDefault(_events);
+
+var _nodeOpcua = require('node-opcua');
+
+var _ProjectConfig = require('../../config/ProjectConfig');
+
+var _ProjectConfig2 = _interopRequireDefault(_ProjectConfig);
+
+var _NodeStream = require('./NodeStream');
+
+var _NodeStream2 = _interopRequireDefault(_NodeStream);
+
+var _QueueStream = require('./QueueStream');
+
+var _QueueStream2 = _interopRequireDefault(_QueueStream);
+
+var _Session = require('./Session');
+
+var _Session2 = _interopRequireDefault(_Session);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * A stream that monitors changes in the read nodes.
+ */
+class SubscribeStream extends _QueueStream2.default {
+
+  /**
+   * Creates a new SubscribeStream based on some options.
+   * @param {Object} options The stream options to apply.
+   */
+  constructor(options = {}) {
+    super(Object.assign(options, { keepSessionAlive: true })); // FIXME: Option not handled.
+
+    /**
+     * Set to true once all nodes are monitored.
+     * @type {Boolean}
+     */
+    this._trackChanges = false;
+
+    this.once('session-open', () => this.createSubscription());
+  }
+
+  /**
+   * Creates the server subscription to monitor nodes with.
+   * @emit {node-opcua~ClientSubscription} Emits a `subscription-started` event once the
+   * subscription started.
+   */
+  createSubscription() {
+    const subscription = new _nodeOpcua.ClientSubscription(this.session, {
+      requestedPublishingInterval: 100,
+      requestedLifetimeCount: 1000,
+      requestedMaxKeepAliveCount: 12,
+      maxNotificationsPerPublish: 10,
+      publishingEnabled: true,
+      priority: 10
+    });
+
+    subscription.on('started', () => {
+      /**
+       * The {@link node-opcua~ClientSubscription} to use to monitor nodes.
+       * @type {node-opcua~ClientSubscription}
+       */
+      this.subscription = subscription;
+      this.emit('subscription-started', subscription);
+    });
+
+    subscription.on('failure', err => this.emit('error', err));
+  }
+
+  /**
+   * Returns an error message specifically for the given reference description.
+   * @param {node-opcua~ReferenceDescription} referenceDescription The reference description to get
+   * the error message for.
+   * @return {String} The specific error message.
+   */
+  processErrorMessage(referenceDescription) {
+    return `Error reading node ${referenceDescription.nodeId.toString()}`;
+  }
+
+  /**
+   * Monitors the nodes specified by a {@link node-opcua~ReferenceDescription}.
+   * @param {node-opcua~ReferenceDescription} referenceDescription The refernce description to use.
+   * @param {function(err: Error, statusCode: node-opcua~StatusCodes, onSuccess: function)}
+   * handleErrors The error handler to call. See {@link QueueStream#processChunk} for details.
+   */
+  processChunk(referenceDescription, handleErrors) {
+    const nodeId = referenceDescription.nodeId;
+
+    const item = this.subscription.monitor({
+      nodeId,
+      attributeId: _nodeOpcua.AttributeIds.Value
+    }, {
+      clientHandle: 13,
+      samplingInterval: 250,
+      queueSize: 1,
+      discardOldest: true
+    }, _nodeOpcua.subscription_service.TimestampsToReturn.Both);
+
+    item.on('changed', dataValue => {
+      if (!this._trackChanges) {
+        handleErrors(null, _nodeOpcua.StatusCodes.Good, done => done()); // Ignore first notification
+      } else {
+        this.emit('change', {
+          nodeId,
+          value: dataValue.value,
+          referenceDescription,
+          mtime: dataValue.serverTimestamp
+        });
+      }
+    });
+
+    item.on('err', err => {
+      /*
+       This works around a bug in node-opcua:
+       Instead of a error a string is emitted
+       FIXME: Remove once bug is fixed
+       */
+      if (err instanceof Error) {
+        handleErrors(err);
+      } else {
+        handleErrors(new Error(err));
+      }
+    });
+  }
+
+  /**
+   * Buffers all read node descriptions until the subscription started, then calls
+   * {@link QueueStream#_enqueueChunk} with them.
+   * @param {node-opcua~ReferenceDescription} desc Reference description of the node to transform.
+   * @param {String} enc The encoding used.
+   * @param {function(err: ?Error)} callback Called with the error that occured while trying to
+   * monitor the given node.
+   */
+  _transform(desc, enc, callback) {
+    if (this.subscription) {
+      this._enqueueChunk(desc);
+      callback();
+    } else {
+      this.once('subscription-started', () => {
+        this._enqueueChunk(desc);
+        callback();
+      });
+    }
+  }
+
+  /**
+   * Starts tracking node changes.
+   * @param {function(err: ?Error)} callback Called once flushing is complete.
+   */
+  _flush(callback) {
+    super._flush(err => {
+      this._trackChanges = true;
+      callback(err);
+    });
+  }
+
+}
+
+exports.SubscribeStream = SubscribeStream; /**
+                                            * Watches the given nodes for value changes.
+                                            * @emit {ReadStream.ReadResult} Emits `change` events when a watched node changes.
+                                            */
+
+class Watcher extends _events2.default {
+
+  /**
+   * Creates a new Watcher with the given nodes.
+   * @param {NodeId[]} nodes The nodes to watch (recursively).
+   */
+  constructor(nodes = _ProjectConfig2.default.nodesToWatch) {
+    super();
+
+    /**
+     * The node stream that discovers the nodes to watch.
+     * @type {NodeStream}
+     */
+    this._nodeStream = new _NodeStream2.default(nodes).on('error', err => this.emit('error', err));
+
+    /**
+     * The stream that starts monitoring the nodes to watch.
+     * @type {SubscribeStream}
+     */
+    this._subscribeStream = new SubscribeStream().on('error', err => this.emit('error', err));
+
+    this._nodeStream.pipe(this._subscribeStream);
+
+    this._subscribeStream.on('finish', () => this.emit('ready'));
+    this._subscribeStream.on('change', event => this.emit('change', event));
+  }
+
+  /**
+   * Ends monitoring nodes.
+   */
+  close() {
+    if (this._subscribeStream.session) {
+      _Session2.default.close(this._subscribeStream.session).catch(err => this.emit('error', err));
+    }
+  }
+
+}
+exports.default = Watcher;
