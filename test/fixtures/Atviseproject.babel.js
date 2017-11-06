ceea6b93b56449d7519cef91849b316aaa0cd853
Lukas Hechenberger <admin@ls-age.com>
2017-11-06 17:22:36 +0100

    revert: ci: Test on gcloud hosted test server
    Reverts ef564089773341181cda6132fc45acbe9074d104

--- test/fixtures/Atviseproject.babel.js
+++ test/fixtures/Atviseproject.babel.js
@@ -3,12 +3,12 @@ import Atviseproject from '../../src/lib/config/Atviseproject';
 export default class TestProject extends Atviseproject {
 
   static get host() {
-    return '35.184.69.89';
+    return '185.67.228.66';
   }
 
   static get port() {
     return {
-      opc: 4840,
+      opc: 4888,
       http: 8888,
     };
   }
