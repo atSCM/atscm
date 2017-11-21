6d95627b77b91ec5c1fe35e54b088951d523afb2
Lukas Hechenberger <LukasHechenberger@users.noreply.github.com>
2017-11-21 11:31:26 +0100

    feat: Store type definitions to files (#71)

---
+++ src/lib/server/NodeFileStream.js
@@ -0,0 +1,73 @@
+import QueueStream from './QueueStream';
+import Logger from 'gulplog';
+import CombinedNodeFile from './CombinedNodeFile';
+import { StatusCodes } from 'node-opcua';
+
+/**
+ * A stream that combines content {@link AtviseFile}s with type definition {@link AtviseFile}s
+ * to {@link CombinedNodeFile}s.
+ */
+export default class NodeFileStream extends QueueStream {
+
+  /**
+   * Creates a new NodeFileStream.
+   */
+  constructor() {
+
+    super();
+
+    /**
+     * The file cache
+     * @type {Map<node-opcua~NodeId, AtviseFile>}
+     */
+    this.combinedFilesCache = {};
+  }
+
+  /**
+   * Returns an error message specifically for the given atvise file
+   * @param {AtviseFile} file The file to get
+   * the error message for.
+   * @return {String} The specific error message.
+   */
+  processErrorMessage(file) {
+    return `Error processing file with node id: ${file.nodeId.toString()}`;
+  }
+
+  /**
+   * Mapps the given content {@link AtviseFiles}s with the given type definition{@link AtviseFiles}s.
+   * @param {AtviseFile} file The file to process
+   * @param {function(err: Error, statusCode: node-opcua~StatusCodes, onSuccess: function)}
+   * handleErrors The error handler to call. See {@link QueueStream#processChunk} for details.
+   */
+
+  processChunk(file, handleErrors) {
+    const nodeId = file.nodeId;
+    let combinedFile = {};
+
+    if (!CombinedNodeFile.hasValidType(file)) {
+      handleErrors(new Error(`NodeFileStream: File has invalid type:  ${nodeId.toString()}`));
+    }
+
+    if (this.combinedFilesCache.hasOwnProperty(nodeId)) {
+      combinedFile = this.combinedFilesCache[nodeId];
+
+      if (!combinedFile.addFile(file)) {
+        handleErrors(new Error(`NodeFileStream: Duplicate file:  ${nodeId.toString()}`));
+      };
+
+      if (combinedFile.isComplete) {
+        handleErrors(null, StatusCodes.Good, done => {
+          this.push(combinedFile);
+          delete this.combinedFilesCache[nodeId];
+          done();
+        });
+      }
+    } else {
+      handleErrors(null, StatusCodes.Good, done => {
+        this.combinedFilesCache[nodeId] = new CombinedNodeFile(file);
+        done();
+      });
+    }
+  }
+}
+
--- src/lib/server/ReadStream.js
+++ src/lib/server/ReadStream.js
@@ -1,4 +1,6 @@
 import QueueStream from './QueueStream';
+import Logger from 'gulplog';
+import { StatusCodes } from 'node-opcua';
 
 /**
  * A stream that reads atvise server nodes for the {@link node-opcua~ReferenceDescription}s passed.
@@ -6,48 +8,50 @@ import QueueStream from './QueueStream';
 export default class ReadStream extends QueueStream {
 
   /**
-   * Returns an error message specifically for the given reference description.
-   * @param {node-opcua~ReferenceDescription} referenceDescription The reference description to get
+   * Returns an error message specifically for the given mapping item.
+   * @param {MappingItem} mappingItem The reference description to get
    * the error message for.
    * @return {String} The specific error message.
    */
-  processErrorMessage(referenceDescription) {
-    return `Error reading node ${referenceDescription.nodeId.toString()}`;
+  processErrorMessage(mappingItem) {
+    return `Error processing item with  ${mappingItem.nodeId.toString()}`;
   }
 
   /**
-   * Returns a {ReadStream.ReadResult} for the given reference description.
-   * @param {node-opcua~ReferenceDescription} referenceDescription The reference description to read
-   * the atvise server node for.
+   * Adds the data of read {node-opcua~DataValue}s for given {@link MappingItem}s.
+   * @param {MappingItem} mappingItem The mappingItem to process
+   * If the proccessed item is not a type definition, the node value will be read from the atvise server
    * @param {function(err: Error, statusCode: node-opcua~StatusCodes, onSuccess: function)}
    * handleErrors The error handler to call. See {@link QueueStream#processChunk} for details.
    */
-  processChunk(referenceDescription, handleErrors) {
-    const nodeId = referenceDescription.nodeId;
 
-    this.session.read([{ nodeId }], (err, nodesToRead, results) => {
-      if (!err && (!results || results.length === 0)) {
-        handleErrors(new Error('No results'));
-      } else {
-        handleErrors(err, results && results.length > 0 ? results[0].statusCode : null, done => {
-          this.push({
-            nodeId,
-            value: results[0].value,
-            referenceDescription,
-            mtime: results[0].sourceTimestamp,
+  processChunk(mappingItem, handleErrors) {
+    let nodeId = mappingItem.readNodeConfig.nodeId;
+
+    if (!mappingItem.isReadNodeConfig) {
+      this.push(mappingItem);
+      handleErrors(null, StatusCodes.Good, done => done());
+    } else {
+
+      this.session.read([{nodeId}], (err, nodesToRead, results) => {
+        if(! err && (! results || results.length === 0)) {
+          handleErrors(new Error('No results'));
+        } else {
+          handleErrors(err, results && results.length > 0 ? results[0].statusCode : null, done => {
+            let dataValue = results[0];
+
+            if (dataValue.value == null) {
+              Logger.warn(`Unable to read value of node:  ${nodeId.toString()}`);
+            } else {
+              mappingItem.addDataValueToReadNodeConfig(dataValue);
+              this.push(mappingItem);
+            }
+
+            done();
           });
-          done();
-        });
-      }
-    });
+        }
+      });
+    }
   }
-
 }
 
-/**
- * @typedef {Object} ReadStream.ReadResult
- * @property {NodeId} nodeId The read node's id.
- * @property {?node-opcua~DataValue} value The read value.
- * @property {Object} referenceDescription Additional info on the read node.
- * @property {Date} mtime The timestamp the node's value last changed.
- */
