6fd6faeac9eba4e5ab658abfc3dbe2174c8bfe35
Lukas Hechenberger <admin@ls-age.com>
2017-11-28 18:34:42 +0100

    test: Prevent circular dependency in configuration
    This may lead to further issues testing the Atviseproject class once all reference issues are resolved

--- test/prepare.js
+++ test/prepare.js
@@ -1,3 +1,17 @@
 import { join } from 'path';
 
-process.env.ATSCM_CONFIG_PATH = join(__dirname, './fixtures/Atviseproject.babel.js');
+function setEnvVarIfNeeded(name, value) {
+  const key = `ATSCM_PROJECT__${name}`;
+
+  if (process.env[key] === undefined) {
+    process.env[key] = value;
+  }
+}
+
+process.env.ATSCM_CONFIG_PATH = join(__dirname, './../src/lib/config/Atviseproject.js');
+
+setEnvVarIfNeeded('HOST', '185.67.228.66');
+setEnvVarIfNeeded('PORT_OPC', '4888');
+setEnvVarIfNeeded('PORT_HTTP', '8888');
+setEnvVarIfNeeded('LOGIN__USERNAME', process.env.ATVISE_USERNAME);
+setEnvVarIfNeeded('LOGIN__PASSWORD', process.env.ATVISE_PASSWORD);
