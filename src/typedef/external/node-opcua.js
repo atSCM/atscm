a2e147f43516b945332bef046ef837eac9057587
Lukas Hechenberger <admin@ls-age.com>
2018-02-08 16:42:06 +0100

    fix(docs): Update links to node-opcua api docs

--- src/typedef/external/node-opcua.js
+++ src/typedef/external/node-opcua.js
@@ -1,33 +1,33 @@
 /**
- * @external {node-opcua} http://node-opcua.github.io/api_doc/
+ * @external {node-opcua} http://node-opcua.github.io/api_doc/0.1.0/
  */
 
 /**
- * @external {node-opcua~NodeId} http://node-opcua.github.io/api_doc/classes/NodeId.html
+ * @external {node-opcua~NodeId} http://node-opcua.github.io/api_doc/0.1.0/classes/NodeId.html
  */
 
 /**
- * @external {node-opcua~NodeIdType} http://node-opcua.github.io/api_doc/classes/NodeIdType.html
+ * @external {node-opcua~NodeIdType} http://node-opcua.github.io/api_doc/0.1.0/classes/NodeIdType.html
  */
 
 /**
- * @external {node-opcua~OPCUAClient} http://node-opcua.github.io/api_doc/classes/OPCUAClient.html
+ * @external {node-opcua~OPCUAClient} http://node-opcua.github.io/api_doc/0.1.0/classes/OPCUAClient.html
  */
 
 /**
- * @external {node-opcua~ClientSession} http://node-opcua.github.io/api_doc/classes/ClientSession.html
+ * @external {node-opcua~ClientSession} http://node-opcua.github.io/api_doc/0.1.0/classes/ClientSession.html
  */
 
 /**
- * @external {node-opcua~ClientSubscription} http://node-opcua.github.io/api_doc/classes/ClientSubscription.html
+ * @external {node-opcua~ClientSubscription} http://node-opcua.github.io/api_doc/0.1.0/classes/ClientSubscription.html
  */
 
 /**
- * @external {node-opcua~DataValue} http://node-opcua.github.io/api_doc/classes/DataValue.html
+ * @external {node-opcua~DataValue} http://node-opcua.github.io/api_doc/0.1.0/classes/DataValue.html
  */
 
 /**
- * @external {node-opcua~ReferenceDescription} http://node-opcua.github.io/api_doc/classes/ReferenceDescription.html
+ * @external {node-opcua~ReferenceDescription} http://node-opcua.github.io/api_doc/0.1.0/classes/ReferenceDescription.html
  */
 
 /**
