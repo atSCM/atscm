17a1dba9e09b567a3b9af8e32b3232965321315d
CircleCI <circleci@circleci>
2018-02-09 15:44:36 +0000

    chore(release): Add 0.5.0 release files [ci skip]

---
+++ out/lib/server/ReadStream.js
@@ -0,0 +1,66 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _QueueStream = require('./QueueStream');
+
+var _QueueStream2 = _interopRequireDefault(_QueueStream);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * A stream that reads atvise server nodes for the {@link node-opcua~ReferenceDescription}s passed.
+ */
+class ReadStream extends _QueueStream2.default {
+
+  /**
+   * Returns an error message specifically for the given reference description.
+   * @param {node-opcua~ReferenceDescription} referenceDescription The reference description to get
+   * the error message for.
+   * @return {string} The specific error message.
+   */
+  processErrorMessage(referenceDescription) {
+    return `Error reading node ${referenceDescription.nodeId.toString()}`;
+  }
+
+  /**
+   * Returns a {ReadStream.ReadResult} for the given reference description.
+   * @param {node-opcua~ReferenceDescription} referenceDescription The reference description to read
+   * the atvise server node for.
+   * @param {function(err: Error, statusCode: node-opcua~StatusCodes, onSuccess: function)}
+   * handleErrors The error handler to call. See {@link QueueStream#processChunk} for details.
+   */
+  processChunk(referenceDescription, handleErrors) {
+    const nodeId = referenceDescription.nodeId;
+
+    this.session.read([{ nodeId }], (err, nodesToRead, results) => {
+      if (!err && (!results || results.length === 0)) {
+        handleErrors(new Error('No results'));
+      } else {
+        handleErrors(err, results && results.length > 0 ? results[0].statusCode : null, done => {
+          this.push({
+            nodeId,
+            value: results[0].value,
+            referenceDescription,
+            mtime: results[0].sourceTimestamp
+          });
+          done();
+        });
+      }
+    });
+  }
+
+}
+
+exports.default = ReadStream; /**
+                               * @typedef {Object} ReadStream.ReadResult
+                               * @property {NodeId} nodeId The read node's id.
+                               * @property {?node-opcua~DataValue} value The read value.
+                               * @property {Object} referenceDescription Additional info on the read node.
+                               * @property {Date} mtime The timestamp the node's value last changed.
+                               */
+/* Needed as long as https://github.com/gajus/eslint-plugin-jsdoc/issues/56 is open */
+/* eslint-disable jsdoc/check-param-names */
+//# sourceMappingURL=ReadStream.js.map
---
+++ out/lib/transform/PartialTransformer.js
@@ -0,0 +1,94 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _gulpFilter = require('gulp-filter');
+
+var _gulpFilter2 = _interopRequireDefault(_gulpFilter);
+
+var _Transformer = require('./Transformer');
+
+var _Transformer2 = _interopRequireDefault(_Transformer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * A transformer that transforms only some of the files read.
+ * @abstract
+ */
+class PartialTransformer extends _Transformer2.default {
+
+  /**
+   * Creates a new partial transformer with the specified options.
+   * @param {Object} options The options to use. See {@link Transformer#constructor} for available
+   * options.
+   */
+  constructor(options = {}) {
+    super(options);
+
+    /**
+     * The filter stream used.
+     * @type {Stream}
+     */
+    this.filter = (0, _gulpFilter2.default)(file => this.shouldBeTransformed(file), { restore: true });
+  }
+
+  /**
+   * `true` if `file` should be transformed.
+   * @param {AtviseFile} file The file to transform or not.
+   * @abstract
+   */
+  shouldBeTransformed(file) {
+    // eslint-disable-line no-unused-vars
+    throw new Error('PartialTransformer#shouldBeTransformed must be implemented by all subclasses');
+  }
+
+  /**
+   * Transforms a file if {@link PartialTransformer#shouldBeTransformed} returns `true`.
+   * @param {AtviseFile} file The file to transform.
+   * @param {string} enc The encoding used.
+   * @param {function(err: ?Error, data: ?AtviseFile)} callback Called with the error that occured
+   * while transforming or (optionally) the transformed file.
+   */
+  _transform(file, enc, callback) {
+    if (this.shouldBeTransformed(file)) {
+      super._transform(file, enc, callback);
+    } else {
+      callback(null, file);
+    }
+  }
+
+  /**
+   * Applies the transformer to the given stream. It does so by running the following steps:
+   *  - Pipe the {@link PartialTransformer#filter} stream
+   *  - If PartialTransformer#applyToFilteredStream is overrridden, apply it.
+   *  - Otherwise pipe {@link Transformer#withDirection}.
+   *  - Restore the filter stream.
+   * @param {Stream} stream The stream to apply the transformer to.
+   * @param {TransformDirection} direction The direction to use.
+   * @return {Stream} The resulting stream.
+   */
+  applyToStream(stream, direction) {
+    const filteredStream = stream.pipe(this.filter);
+
+    return (this.applyToFilteredStream(filteredStream, direction) || filteredStream.pipe(this.withDirection(direction))).pipe(this.filter.restore);
+  }
+
+  /**
+   * Applies a stream transformer to the given, already filtered stream. Override this method if you
+   * want to pipe streams directly. Returning a falsy value (false, null, undefined, ...) invokes
+   * {@link Transformer#transformFromDB} or {@link Transformer#transformFromFilesystem} instead.
+   * @param {Stream} stream The stream to apply the transformer to.
+   * @param {TransformDirection} direction The direction to use.
+   * @return {?Stream} The resulting stream.
+   */
+  applyToFilteredStream(stream, direction) {
+    // eslint-disable-line no-unused-vars
+    return false;
+  }
+
+}
+exports.default = PartialTransformer;
+//# sourceMappingURL=PartialTransformer.js.map
