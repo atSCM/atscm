607129234043d86d1068ee074b54839306fa7cb1
CircleCI <circleci@circleci>
2018-03-09 09:51:44 +0000

    chore(release): Add 0.6.0-beta.12 release files [ci skip]

---
+++ out/tasks/push.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = push;
+
+var _gulp = require('gulp');
+
+var _PushStream = require('../lib/gulp/PushStream');
+
+var _PushStream2 = _interopRequireDefault(_PushStream);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Pushes {@link AtviseFile}s to atvise server.
+ */
+function push() {
+  return new _PushStream2.default((0, _gulp.src)(['./src/**/.*', '!./src/**/.*.rc', './src/**/*.*']));
+}
+
+push.description = 'Push all stored nodes to atvise server';
+//# sourceMappingURL=push.js.map
