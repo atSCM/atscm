3e866e21cf4ab350db83ebcb745aaf547a43dd6b
Lukas Hechenberger <admin@ls-age.com>
2018-03-30 13:35:13 +0200

    test: Wait for remote notification

--- test/integration/issues/issue-157.spec.js
+++ test/integration/issues/issue-157.spec.js
@@ -4,12 +4,19 @@ import { importSetup, callScript } from '../../helpers/atscm';
 
 describe('watch task', function() {
   context('when a watched node is deleted', function() {
-    it('should not error', async function() {
+    it('should not error', async function(cb) {
       const nodeName = await importSetup('issue-157', 'DeleteDisplay');
       const nodeId = `AGENT.DISPLAYS.${nodeName}`;
 
       // Start watch task
-      await watch({ open: false });
+      const { serverWatcher } = await watch({ open: false });
+
+      // Wait until deletion is recognized
+      serverWatcher.on('change', r => {
+        if (r.nodeId.value.split(nodeId).length > 1) {
+          cb();
+        }
+      });
 
       // Delete the node
       await callScript('DeleteNode', {
