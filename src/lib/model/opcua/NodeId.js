33d20eebd078051d81dfaf3b541177d931527b1b
Lukas Hechenberger <LukasHechenberger@users.noreply.github.com>
2018-05-10 15:17:42 +0200

    fix: Process first path component

--- src/lib/model/opcua/NodeId.js
+++ src/lib/model/opcua/NodeId.js
@@ -67,14 +67,14 @@ export default class NodeId extends OpcNodeId {
     let separator = '.';
     const value = path.split(sep)
       .reduce((result, current) => {
-        const next = `${result}${separator}${current.replace('%2F', '/')}`;
+        const next = `${result ? `${result}${separator}` : ''}${current.replace('%2F', '/')}`;
 
         if (current === 'RESOURCES') {
           separator = '/';
         }
 
         return next;
-      });
+      }, '');
 
     return new NodeId(NodeId.NodeIdType.STRING, value, 1);
   }
