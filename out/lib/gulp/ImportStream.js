26ae63d63ab2cf53a8926e5c70ba076a60ae6bb2
CircleCI <circleci@circleci>
2018-05-10 12:25:30 +0000

    chore(release): Add 0.6.0-beta.30 release files [ci skip]

---
+++ out/lib/gulp/ImportStream.js
@@ -0,0 +1,89 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _nodeOpcua = require('node-opcua');
+
+var _gulplog = require('gulplog');
+
+var _gulplog2 = _interopRequireDefault(_gulplog);
+
+var _NodeId = require('../model/opcua/NodeId');
+
+var _NodeId2 = _interopRequireDefault(_NodeId);
+
+var _CallMethodStream = require('../server/scripts/CallMethodStream');
+
+var _CallMethodStream2 = _interopRequireDefault(_CallMethodStream);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * The import operation's scope, which is set to be *absolute*.
+ * @type {NodeId}
+ */
+const scopeId = new _NodeId2.default(_NodeId2.default.NodeIdType.NUMERIC, 0, 0);
+
+/**
+ * A stream that imports xml files in parallel.
+ */
+class ImportStream extends _CallMethodStream2.default {
+
+  /**
+   * Id of the `importNodes` OPC-UA method.
+   * @type {NodeId}
+   */
+  get methodId() {
+    return new _NodeId2.default('ns=1;s=AGENT.OPCUA.METHODS.importNodes');
+  }
+
+  /**
+   * Returns the arguments the `importNodes` needs to be called with for the given file.
+   * @param {vinyl~File} file The file being processed.
+   * @return {node-opcua~Variant[]} The arguments for the `importNodes` method:
+   *  - The import scope (which is set to be absolute)
+   *  - The XML code (read from *file*)
+   */
+  inputArguments(file) {
+    return [{
+      dataType: _nodeOpcua.DataType.NodeId,
+      value: scopeId
+    }, {
+      dataType: _nodeOpcua.DataType.XmlElement,
+      value: file.contents
+    }];
+  }
+
+  /**
+   * Returns an error message specifically for the given file.
+   * @param {vinyl~File} file The file to generate the error message for.
+   * @return {string} The specific error message.
+   */
+  processErrorMessage(file) {
+    return `Error importing file: ${file.relative}`;
+  }
+
+  /**
+   * Checks if the import succeeded and calls `callback` with an error otherwise.
+   * @param {vinyl~File} file The file that was processed.
+   * @param {?node-opcua~Variant[]} outputArguments The import status output arguments (Array with a
+   * single entry).
+   * @param {function(err: ?Error)} callback The callback called with an error if import failed.
+   */
+  handleOutputArguments(file, outputArguments, callback) {
+    const [importStatus] = outputArguments || [{}];
+
+    if (importStatus.value) {
+      _gulplog2.default.debug(`Successfully imported file: ${file.relative}`);
+
+      callback(null);
+    } else {
+      callback(new Error('Import failed'));
+    }
+  }
+
+}
+exports.default = ImportStream;
+//# sourceMappingURL=ImportStream.js.map
