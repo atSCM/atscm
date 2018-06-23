d2251731a0ec3538c569ea8a7406038ebef9c95b
Lukas Hechenberger <admin@ls-age.com>
2018-06-23 13:29:56 +0200

    style(eslint): Remove unused imports

--- src/lib/gulp/PushStream.js
+++ src/lib/gulp/PushStream.js
@@ -6,7 +6,6 @@ import MappingTransformer from '../../transform/Mapping';
 import WriteStream from '../server/WriteStream';
 import CreateNodeStream from '../server/CreateNodeStream';
 import AddReferencesStream from '../server/AddReferencesStream';
-import NewlinesTransformer from '../../transform/Newlines';
 
 /**
  * A stream that transforms read {@link vinyl~File}s and pushes them to atvise server.
