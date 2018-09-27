e639a49b4305467de2aa2ec214339bfe7a9f3037
Lukas Hechenberger <admin@ls-age.com>
2018-09-27 11:41:40 +0200

    feat(pull): Use `--clean` to remove existing files

--- src/tasks/pull.js
+++ src/tasks/pull.js
@@ -1,3 +1,7 @@
+import parseOptions from 'mri';
+import { emptyDir } from 'fs-extra';
+import toPromise from 'stream-to-promise';
+import Logger from 'gulplog';
 import ProjectConfig from '../config/ProjectConfig';
 import NodeStream from '../lib/server/NodeStream';
 import PullStream from '../lib/gulp/PullStream';
@@ -6,9 +10,20 @@ import PullStream from '../lib/gulp/PullStream';
  * Pulls all nodes from atvise server.
  */
 export default function pull() {
-  return new PullStream(
-    (new NodeStream(ProjectConfig.nodes))
-  );
+  const { clean } = parseOptions(process.argv.slice(2));
+
+  return Promise.resolve()
+    .then(() => {
+      if (clean) {
+        Logger.info('Using --clean, removing pulled files first');
+        return emptyDir('./src');
+      }
+
+      return Promise.resolve();
+    })
+    .then(() => toPromise(new PullStream(
+      (new NodeStream(ProjectConfig.nodes))
+    )));
 }
 
 pull.description = 'Pull all nodes from atvise server';
