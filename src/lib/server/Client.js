084137b16b16c5e62797c532c1b12a753bc54194
Lukas Hechenberger <admin@ls-age.com>
2018-09-27 17:12:41 +0200

    style: Rename certificate files

--- src/lib/server/Client.js
+++ src/lib/server/Client.js
@@ -16,8 +16,8 @@ export default class Client {
     const client = new OPCUAClient({
       requestedSessionTimeout: 600000,
       keepSessionAlive: true,
-      certificateFile: join(__dirname, '../../../res/certificates/client_selfsigned_cert_1024.pem'),
-      privateKeyFile: join(__dirname, '../../../res/certificates/client_key_1024.pem'),
+      certificateFile: join(__dirname, '../../../res/certificates/certificate.pem'),
+      privateKeyFile: join(__dirname, '../../../res/certificates/key.pem'),
     });
 
     return new Promise((resolve, reject) => {
