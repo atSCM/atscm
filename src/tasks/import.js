84c843afaedb6229c6e3b18f61b6ded2a0d6c5a3
Lukas Hechenberger <LukasHechenberger@users.noreply.github.com>
2018-10-25 16:46:45 +0200

    feat: Check server scripts version on push (#230)
    This prevents server scripts to be outdated/buggy

--- src/tasks/import.js
+++ src/tasks/import.js
@@ -1,16 +1,35 @@
 import { src } from 'gulp';
 import scripts from '@atscm/server-scripts';
+import { version } from '@atscm/server-scripts/package.json';
+import toPromise from 'stream-to-promise';
+import { DataType } from 'node-opcua/lib/datamodel/variant';
+import { StatusCodes } from 'node-opcua/lib/datamodel/opcua_status_code';
 import ImportStream from '../lib/gulp/ImportStream';
+import { writeNode, createNode } from '../api';
+import { versionNode } from '../lib/server/scripts/version';
 
 /**
  * Imports all xml files needed for atscm usage.
- * @return {ImportStream} The import stream used.
+ * @return {Promise<void>} The running task.
  */
 export default function importTask() {
   const srcStream = src(scripts);
+  const versionVariant = { dataType: DataType.String, value: version };
 
-  return srcStream
-    .pipe(new ImportStream());
+  return toPromise(srcStream
+    .pipe(new ImportStream())
+  )
+    .then(() => writeNode(versionNode, versionVariant))
+    .catch(err => {
+      if (err.statusCode === StatusCodes.BadNodeIdUnknown) {
+        return createNode(versionNode, {
+          name: 'version',
+          value: versionVariant,
+        });
+      }
+
+      throw err;
+    });
 }
 
 importTask.description = 'Imports all xml resources needed for atscm usage';
