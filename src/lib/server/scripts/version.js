e8fce7ad24b524642e3faa3b479bb7afd4462e4c
Lukas Hechenberger <admin@ls-age.com>
2018-10-25 16:26:08 +0200

    docs: Add missing annotations

--- src/lib/server/scripts/version.js
+++ src/lib/server/scripts/version.js
@@ -2,4 +2,8 @@
 
 import NodeId from '../../model/opcua/NodeId';
 
+/**
+ * The node containing the currently installed server-scripts version.
+ * @type {NodeId}
+ */
 export const versionNode = new NodeId('SYSTEM.LIBRARY.ATVISE.SERVERSCRIPTS.atscm.version');
