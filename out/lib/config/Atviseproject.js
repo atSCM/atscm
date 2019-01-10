4d0d6ccce7aaed65212eef8697b79218dc7dc87f
CircleCI <circleci@circleci>
2019-01-10 21:09:42 +0000

    chore(release): Add 1.0.0-beta.16 release files [ci skip]

---
+++ out/lib/config/Atviseproject.js
@@ -0,0 +1,149 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _NodeId = _interopRequireDefault(require("../model/opcua/NodeId"));
+
+var _DisplayTransformer = _interopRequireDefault(require("../../transform/DisplayTransformer"));
+
+var _ScriptTransformer = _interopRequireDefault(require("../../transform/ScriptTransformer"));
+
+var _Mapping = _interopRequireDefault(require("../../transform/Mapping"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* eslint-disable no-useless-escape */
+
+/**
+ * An *atvise-scm* project's configuration.
+ * @abstract
+ */
+class Atviseproject {
+  /**
+   * The atvise-server's host. Defaults to *localhost*.
+   * @type {string}
+   */
+  static get host() {
+    return 'localhost';
+  }
+  /**
+   * The atvise-server ports to use.
+   * @type {Object}
+   * @property {number} [opc=4840] The OPC-UA port the atvise-server runs on.
+   * @property {number} [http=80] The HTTP port the atvise-server can be reached at.
+   */
+
+
+  static get port() {
+    return {
+      opc: 4840,
+      http: 80
+    };
+  }
+  /**
+   * The login to use. Return `false` if no login is required (default).
+   * @type {Boolean|Object}
+   * @property {string} username The username to log in with.
+   * @property {string} password The password to log in with.
+   */
+
+
+  static get login() {
+    return false;
+  }
+  /**
+   * The transformers to use in this project. Returns a {@link DisplayTransformer}, a
+   * {@link ScriptTransformer} and a {@link NewlinesTransformer} by default.
+   * @type {Transformer[]}
+   */
+
+
+  static get useTransformers() {
+    return [new _DisplayTransformer.default(), new _ScriptTransformer.default(), new _Mapping.default()];
+  }
+  /**
+   * The atvise-server nodes that atvise-scm should sync. Defaults to the nodes
+   * *AGENT*, *SYSTEM*, *ObjectTypes.PROJECT* and *VariableTypes.PROJECT*.
+   * @type {String[]|NodeId[]}
+   */
+
+
+  static get nodes() {
+    return [new _NodeId.default('AGENT'), new _NodeId.default('SYSTEM'), new _NodeId.default('ObjectTypes.PROJECT'), new _NodeId.default('VariableTypes.PROJECT')];
+  }
+  /**
+   * The atvise-server nodes to watch in the corresponding tasks. Defaults to all nodes containing
+   * displays.
+   * @type {String[]|NodeId[]}
+   */
+
+
+  static get nodesToWatch() {
+    return [new _NodeId.default('AGENT.DISPLAYS'), new _NodeId.default('SYSTEM.LIBRARY.PROJECT.OBJECTDISPLAYS')];
+  }
+  /**
+   * An array of editor related node ids. They should be ignored in a atvise-scm project.
+   * @type {NodeId[]}
+   */
+
+
+  static get EditorRelatedNodes() {
+    return [new _NodeId.default('SYSTEM\.JOURNALS\.ProjectHistory')];
+  }
+  /**
+   * An array of server related node ids. They should be ignored in a atvise-scm project
+   * as they are read-only.
+   * @type {NodeId[]}
+   */
+
+
+  static get ServerRelatedNodes() {
+    return [new _NodeId.default('AGENT\.OPCUA\.server_url'), new _NodeId.default('AGENT\.WEBACCESS\.https?[^\.]+\.(state)'), new _NodeId.default('SYSTEM\.INFORMATION\.LOGS\.'), new _NodeId.default('AGENT\.GENERATOR\.METHODS'), new _NodeId.default('AGENT\.MIRROR\.METHODS'), new _NodeId.default('AGENT\.HISTORY\.METHODS'), new _NodeId.default('AGENT\.SCRIPT\.METHODS'), new _NodeId.default('AGENT\.OPCUA\.METHODS')];
+  }
+  /**
+   * These nodes (and their subnodes, if any) will be ignored by atvise-scm. Defaults to
+   * {@link Atviseproject.EditorRelatedNodes} combined with
+   * {@link Atviseproject.ServerRelatedNodes}.
+   * @type {NodeId[]}
+   */
+
+
+  static get ignoreNodes() {
+    return this.EditorRelatedNodes.concat(this.ServerRelatedNodes);
+  }
+  /**
+   * Returns an object containing the properties to inspect.
+   * @see https://nodejs.org/api/util.html#util_util_inspect_object_options
+   * @return {Object} The object to inspect.
+   */
+
+
+  static inspect() {
+    return {
+      host: this.host,
+      port: this.port,
+      login: this.login,
+      useTransformers: this.useTransformers,
+      nodes: this.nodes,
+      nodesToWatch: this.nodesToWatch,
+      ignoreNodes: this.ignoreNodes
+    };
+  }
+  /**
+   * The *version control system* to optimize tasks for.
+   * @type {'git' | 'svn'}
+   * @since 1.0.0
+   */
+
+
+  static get vcs() {
+    return 'git';
+  }
+
+}
+
+exports.default = Atviseproject;
+//# sourceMappingURL=Atviseproject.js.map
---
+++ out/transform/DisplayTransformer.js
@@ -0,0 +1,296 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _path = require("path");
+
+var _nodeclass = require("node-opcua/lib/datamodel/nodeclass");
+
+var _variant = require("node-opcua/lib/datamodel/variant");
+
+var _gulplog = _interopRequireDefault(require("gulplog"));
+
+var _XMLTransformer = _interopRequireDefault(require("../lib/transform/XMLTransformer"));
+
+var _xml = require("../lib/helpers/xml");
+
+var _Node = require("../lib/model/Node");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Names of the tags to come before <metadata> in output files.
+ * @type {Set<string>}
+ */
+const tagsBeforeMetadata = new Set(['defs', 'desc', 'title']);
+/**
+ * Returns the index at which the <metadata> section should be inserted in the resulting xml.
+ * @param {Object[]} elements The elements to look at.
+ * @return {number} The insertion index.
+ */
+
+function metadataIndex(elements) {
+  let index = 0;
+
+  while (elements.length > index && tagsBeforeMetadata.has(elements[index].name)) {
+    index += 1;
+  }
+
+  return index;
+}
+/**
+ * Splits read atvise display XML nodes into their SVG and JavaScript sources,
+ * alongside with a .json file containing the display's parameters.
+ */
+
+
+class DisplayTransformer extends _XMLTransformer.default {
+  /**
+   * Returns true for all nodes containing atvise displays.
+   * @param {Node} node The node to check.
+   * @return {boolean} `true` for all atvise display nodes.
+   */
+  shouldBeTransformed(node) {
+    return node.hasTypeDefinition('VariableTypes.ATVISE.Display') || super.shouldBeTransformed(node);
+  }
+  /**
+   * Splits any read files containing atvise displays into their SVG and JavaScript sources,
+   * alongside with a json file containing the display's parameters.
+   * @param {Node} node The display node to split.
+   * @param {string} enc The encoding used.
+   * @param {function(err: Error, node: Node)} callback Called with the error that occured
+   * while transforming the display, or the node passed through.
+   */
+
+
+  transformFromDB(node, enc, callback) {
+    if (node.arrayType !== _variant.VariantArrayType.Scalar) {
+      // FIXME: Instead of throwing we could simply pass the original node to the callback
+      throw new Error('Array of displays not supported');
+    }
+
+    node.markAsResolved('nodeClass');
+    node.markAsResolved('dataType');
+    node.markAsResolved('arrayType');
+    node.markReferenceAsResolved('HasTypeDefinition', 'VariableTypes.ATVISE.Display');
+    this.decodeContents(node, (err, results) => {
+      if (err) {
+        callback(err);
+      } else if (!results) {
+        callback(new Error('Error parsing display: No `svg` tag'));
+      } else {
+        const xml = results;
+        const document = (0, _xml.findChild)(xml, 'svg');
+
+        if (!document) {
+          callback(new Error('Error parsing display: No `svg` tag'));
+          return;
+        }
+
+        const config = {};
+        const scriptTags = (0, _xml.removeChildren)(document, 'script');
+        let inlineScript = false; // Extract JavaScript
+
+        if (scriptTags.length) {
+          scriptTags.forEach(script => {
+            if (script.attributes && (script.attributes.src || script.attributes['xlink:href'])) {
+              if (!config.dependencies) {
+                config.dependencies = [];
+              }
+
+              config.dependencies.push(script.attributes.src || script.attributes['xlink:href']);
+            } else {
+              // Warn on multiple inline scripts
+              if (inlineScript) {
+                _gulplog.default.warn(`'${node.id.value}' contains multiple inline scripts.`);
+
+                document.elements.push(inlineScript);
+              }
+
+              inlineScript = script;
+            }
+          });
+        }
+
+        if (inlineScript) {
+          const contentNode = inlineScript.elements ? inlineScript.elements[0] : (0, _xml.createTextNode)();
+          const scriptFile = DisplayTransformer.splitFile(node, '.js');
+          const scriptText = contentNode[contentNode.type] || '';
+          scriptFile.value = {
+            dataType: _variant.DataType.String,
+            arrayType: _variant.VariantArrayType.Scalar,
+            value: scriptText
+          };
+          this.push(scriptFile);
+        } // Extract metadata
+
+
+        const metaTag = (0, _xml.findChild)(document, 'metadata');
+
+        if (metaTag && metaTag.elements) {
+          // TODO: Warn on multiple metadata tags
+          // - Parameters
+          const paramTags = (0, _xml.removeChildren)(metaTag, 'atv:parameter');
+
+          if (paramTags.length) {
+            config.parameters = [];
+            paramTags.forEach(({
+              attributes
+            }) => config.parameters.push(attributes));
+          }
+        }
+
+        const configFile = DisplayTransformer.splitFile(node, '.json');
+        configFile.value = {
+          dataType: _variant.DataType.String,
+          arrayType: _variant.VariantArrayType.Scalar,
+          value: JSON.stringify(config, null, '  ')
+        };
+        this.push(configFile);
+        const svgFile = DisplayTransformer.splitFile(node, '.svg');
+        this.encodeContents(xml, (encodeErr, stringValue) => {
+          if (encodeErr) {
+            callback(encodeErr);
+          } else {
+            svgFile.value = {
+              dataType: _variant.DataType.String,
+              arrayType: _variant.VariantArrayType.Scalar,
+              value: stringValue
+            };
+            this.push(svgFile);
+            node.renameTo(`${node.name}.display`);
+            callback(null, node);
+          }
+        });
+      }
+    });
+  }
+  /**
+   * Creates a display from the collected source nodes.
+   * @param {Node} node The node to map to.
+   * @param {Map<String, Node>} sources The collected files, stored against their extension.
+   * @param {function(err: ?Error, data: Node)} callback Called with the error that occured
+   * while creating the display or the resulting file.
+   */
+
+
+  createCombinedFile(node, sources, callback) {
+    /* eslint-disable no-param-reassign */
+    // FIXME: Set nodeClass to NodeClass.Variable
+    node.nodeClass = _nodeclass.NodeClass.Variable;
+    node.markAsResolved('nodeClass'); // Set dataType to DataType.XmlElement
+
+    node.value.dataType = _variant.DataType.XmlElement;
+    node.markAsResolved('dataType'); // Set arrayType to 'Scalar'
+
+    node.value.arrayType = _variant.VariantArrayType.Scalar;
+    node.markAsResolved('arrayType'); // Set type definition reference to 'VariableTypes.ATVISE.Display'
+
+    node.setReferences(_Node.ReferenceTypeIds.HasTypeDefinition, ['VariableTypes.ATVISE.Display']);
+    node.markAllReferencesAsResolved('HasTypeDefinition');
+    /* eslint-enable no-param-reassign */
+
+    node.renameTo((0, _path.basename)(node.name, '.display'));
+    const configFile = sources['.json'];
+    let config = {};
+
+    if (configFile) {
+      try {
+        config = JSON.parse(configFile.stringValue);
+      } catch (e) {
+        callback(new Error(`Error parsing JSON in ${configFile.relative}: ${e.message}`));
+        return;
+      }
+    }
+
+    const svgFile = sources['.svg'];
+
+    if (!svgFile) {
+      callback(new Error(`No display SVG in ${svgFile.path}`));
+      return;
+    }
+
+    const scriptFile = sources['.js'];
+    let inlineScript = '';
+
+    if (scriptFile) {
+      inlineScript = scriptFile.stringValue;
+    }
+
+    this.decodeContents(svgFile, (err, xml) => {
+      if (err) {
+        callback(err);
+      } else {
+        const result = xml;
+        const svg = (0, _xml.findChild)(result, 'svg');
+
+        if (!svg) {
+          callback(new Error('Error parsing display SVG: No `svg` tag'));
+          return;
+        } // Handle empty svg tag
+
+
+        if (!svg.elements) {
+          svg.elements = [];
+        } // Insert dependencies
+
+
+        if (config.dependencies) {
+          config.dependencies.forEach(src => {
+            svg.elements.push((0, _xml.createElement)('script', undefined, {
+              'xlink:href': src
+            }));
+          });
+        } // Insert script
+        // FIXME: Import order is not preserved!
+
+
+        if (scriptFile) {
+          svg.elements.push((0, _xml.createElement)('script', [(0, _xml.createCDataNode)(inlineScript)], {
+            type: 'text/ecmascript'
+          }));
+        } // Insert metadata
+        // - Parameters
+
+
+        if (config.parameters && config.parameters.length > 0) {
+          let metaTag = (0, _xml.removeChild)(svg, 'metadata');
+
+          if (!metaTag) {
+            metaTag = (0, _xml.createElement)('metadata');
+          }
+
+          if (!metaTag.elements) {
+            metaTag.elements = [];
+          } // Parameters should come before other atv attributes, e.g. `atv:gridconfig`
+
+
+          for (let i = config.parameters.length - 1; i >= 0; i--) {
+            metaTag.elements.unshift((0, _xml.createElement)('atv:parameter', undefined, config.parameters[i]));
+          } // Insert <metadata> as first element in the resulting svg, after <defs>, <desc> and
+          // <title> if defined
+
+
+          svg.elements.splice(metadataIndex(svg.elements), 0, metaTag);
+        }
+
+        this.encodeContents(result, (encodeErr, xmlString) => {
+          if (encodeErr) {
+            callback(encodeErr);
+          } else {
+            // eslint-disable-next-line no-param-reassign
+            node.value.value = xmlString;
+            callback(null, node);
+          }
+        });
+      }
+    });
+  }
+
+}
+
+exports.default = DisplayTransformer;
+//# sourceMappingURL=DisplayTransformer.js.map
