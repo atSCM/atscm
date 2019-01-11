0c018604e2ce602061e8f9f60197dafd4de83f57
CircleCI <circleci@circleci>
2019-01-11 09:38:36 +0000

    chore(release): Add 1.0.0-beta.17 release files [ci skip]

---
+++ out/index.js
@@ -0,0 +1,88 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Atviseproject", {
+  enumerable: true,
+  get: function () {
+    return _Atviseproject.default;
+  }
+});
+Object.defineProperty(exports, "ProjectConfig", {
+  enumerable: true,
+  get: function () {
+    return _ProjectConfig.default;
+  }
+});
+Object.defineProperty(exports, "NodeId", {
+  enumerable: true,
+  get: function () {
+    return _NodeId.default;
+  }
+});
+Object.defineProperty(exports, "Transformer", {
+  enumerable: true,
+  get: function () {
+    return _Transformer.default;
+  }
+});
+Object.defineProperty(exports, "TransformDirection", {
+  enumerable: true,
+  get: function () {
+    return _Transformer.TransformDirection;
+  }
+});
+Object.defineProperty(exports, "PartialTransformer", {
+  enumerable: true,
+  get: function () {
+    return _PartialTransformer.default;
+  }
+});
+Object.defineProperty(exports, "SplittingTransformer", {
+  enumerable: true,
+  get: function () {
+    return _SplittingTransformer.default;
+  }
+});
+Object.defineProperty(exports, "DisplayTransformer", {
+  enumerable: true,
+  get: function () {
+    return _DisplayTransformer.default;
+  }
+});
+Object.defineProperty(exports, "ScriptTransformer", {
+  enumerable: true,
+  get: function () {
+    return _ScriptTransformer.default;
+  }
+});
+Object.defineProperty(exports, "NewlinesTransformer", {
+  enumerable: true,
+  get: function () {
+    return _Newlines.default;
+  }
+});
+
+var _Atviseproject = _interopRequireDefault(require("./lib/config/Atviseproject"));
+
+var _ProjectConfig = _interopRequireDefault(require("./config/ProjectConfig"));
+
+var _NodeId = _interopRequireDefault(require("./lib/model/opcua/NodeId"));
+
+var _Transformer = _interopRequireWildcard(require("./lib/transform/Transformer"));
+
+var _PartialTransformer = _interopRequireDefault(require("./lib/transform/PartialTransformer"));
+
+var _SplittingTransformer = _interopRequireDefault(require("./lib/transform/SplittingTransformer"));
+
+var _DisplayTransformer = _interopRequireDefault(require("./transform/DisplayTransformer"));
+
+var _ScriptTransformer = _interopRequireDefault(require("./transform/ScriptTransformer"));
+
+var _Newlines = _interopRequireDefault(require("./transform/Newlines"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//# sourceMappingURL=index.js.map
---
+++ out/lib/server/WaitingStream.js
@@ -0,0 +1,154 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = exports.waitForDependencies = void 0;
+
+var _gulplog = _interopRequireDefault(require("gulplog"));
+
+var _ProjectConfig = _interopRequireDefault(require("../../config/ProjectConfig"));
+
+var _QueueStream = _interopRequireDefault(require("./QueueStream"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * A mixin that transforms a regular stream into a {@link WaitingStream}, which makes it wait for a
+ * node's dependencies to be transformed.
+ * @param {QueueStream} Stream The node stream to inherit from.
+ * @return {Class<WaitingStream>} The resulting stream class.
+ * @example <caption>Basic usage</caption>
+ * import QueueStream from 'atscm/src/lib/server/QueueStream';
+ * import { waitForDependencies } from 'atscm/src/lib/server/WaitingStream';
+ *
+ * class MyWaitingStream extends waitForDependencies(QueueStream) {
+ *
+ *   dependenciesFor(file) {
+ *     return [
+ *       // An array of node ids
+ *     ];
+ *   }
+ *
+ * }
+ */
+const waitForDependencies = Stream => class Waiting extends Stream {
+  /**
+   * Creates a new WaitingStream.
+   * @param {Object} options Options passed to the underlying {@link Stream} subclass.
+   */
+  constructor(options = {}) {
+    super(options);
+    this._dependencies = {};
+    this._waitingFor = {};
+    this._finishedProcessing = {};
+    this.on('processed-chunk', file => {
+      const key = file.nodeId.toString();
+      const dependents = this._waitingFor[key];
+      delete this._waitingFor[key];
+      this._finishedProcessing[key] = true;
+
+      if (dependents) {
+        dependents.forEach(d => {
+          const k = d.nodeId.toString();
+          this._dependencies[k] -= 1;
+
+          if (this._dependencies[k] === 0) {
+            super._enqueueChunk(d);
+
+            delete this._dependencies[k];
+          }
+        });
+      }
+
+      this.emit('finished-chunk');
+    });
+  }
+  /**
+   * **Must be implemented by all subclasses:** Returns the dependencies for a given file.
+   * @typedef {function(file: AtviseFile): NodeId[]} WWaiting#dependenciesFor
+   * @param {AtviseFile} file The file to get the dependencies for.
+   * @return {NodeId[]} The file's dependencies.
+   * @abstract
+   */
+
+
+  dependenciesFor(file) {
+    // eslint-disable-line no-unused-vars
+    throw new Error('#dependenciesFor must be implemented by all subclasses');
+  }
+  /**
+   * Enqueues a file after it's dependencies.
+   * @param {AtviseFile} file The file to process.
+   */
+
+
+  _enqueueChunk(file) {
+    const dependencies = this.dependenciesFor(file).filter(id => {
+      if (id.namespace === 0 || !id.value || _ProjectConfig.default.nodes.includes(id)) {
+        return false;
+      }
+
+      return !id.value.match(/^(Object|Variable)Types$/) && !id.value.match(/^(Object|Variable)Types\.ATVISE/) && !id.value.match(/^SYSTEM\.LIBRARY\.ATVISE/);
+    }).map(id => id.toString());
+
+    if (dependencies && dependencies.length) {
+      const needToWait = dependencies.reduce((wait, dependency) => {
+        if (!this._finishedProcessing[dependency]) {
+          const name = file.nodeId.toString();
+          this._waitingFor[dependency] = (this._waitingFor[dependency] || []).concat(file);
+          this._dependencies[name] = (this._dependencies[name] || 0) + 1;
+          return true;
+        }
+
+        return wait;
+      }, false);
+
+      if (needToWait) {
+        return;
+      }
+    }
+
+    super._enqueueChunk(file);
+  }
+  /**
+   * Delays the streams end until all chunks have been processed.
+   * @param {function(err: Error)} callback Called once all chunks have been processed.
+   */
+
+
+  _flush(callback) {
+    const checkProcessing = () => {
+      if (this._processing || this.hasPending) {
+        this.once('finished-chunk', () => checkProcessing());
+      } else if (Object.keys(this._waitingFor).length > 0) {
+        const first = Object.keys(this._waitingFor)[0];
+
+        _gulplog.default.debug(`Missing dependency. Trying to process dependents of ${first}`);
+
+        this.once('finished-chunk', () => checkProcessing());
+        this.emit('processed-chunk', {
+          nodeId: first
+        });
+      } else {
+        super._flush(callback);
+      }
+    };
+
+    checkProcessing();
+  }
+
+};
+/**
+ * A {@link QueueStream} that waits for a file's dependencies to be processed before the file is
+ * processed itself.
+ * @abstract
+ */
+
+
+exports.waitForDependencies = waitForDependencies;
+
+class WaitingStream extends waitForDependencies(_QueueStream.default) {}
+
+exports.default = WaitingStream;
+//# sourceMappingURL=WaitingStream.js.map
