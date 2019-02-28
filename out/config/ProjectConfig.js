7b1275c41bc7a77a9e6ed39958bd63b061bc5311
CircleCI <circleci@circleci>
2019-02-28 14:01:50 +0000

    chore(release): Add 1.0.0-beta.22 release files [ci skip]

---
+++ out/config/ProjectConfig.js
@@ -0,0 +1,85 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = exports.path = void 0;
+
+/**
+ * The path to the project's configuration file.
+ * @type {string}
+ */
+const path = process.env.ATSCM_CONFIG_PATH;
+/**
+ * The current project's configuration.
+ * @type {Atviseproject}
+ */
+
+exports.path = path;
+
+const Config = require(path).default;
+/**
+ * The current project's configuration, with overrides (through `ATSCM_PROJECT__` env vars) already
+ * handled.
+ */
+
+
+class ProjectConfig extends Config {
+  /**
+   * Return the project configuration override for the given name if available.
+   * @param {string} name The variable to return.
+   * @type {string|undefined} The variables's value or `undefined`.
+   */
+  static _env(name) {
+    return process.env[`ATSCM_PROJECT__${name}`];
+  }
+  /**
+   * The atvise server's host. Can be overridden with the `ATSCM_PROJECT__HOST` env variable.
+   * @type {string}
+   */
+
+
+  static get host() {
+    return this._env('HOST') || super.host;
+  }
+  /**
+   * The atvise server ports to use.
+   * @type {Object}
+   * @property {number} opc The OPC-UA port the atvise server runs on. Can be overridden with the
+   * `ATSCM_PROJECT__PORT__OPC` env variable.
+   * @property {number} http The HTTP port the atvise server can be reached at. Can be overridden
+   * with the `ATSCM_PROJECT__PORT__HTTP` env variable.
+   */
+
+
+  static get port() {
+    return {
+      opc: parseInt(this._env('PORT__OPC'), 10) || super.port.opc,
+      http: parseInt(this._env('PORT__HTTP'), 10) || super.port.http
+    };
+  }
+  /**
+   * The login to use. Return false if no login is required (default).
+   * @type {Object}
+   * @property {string} username The username to log in with. Can be overridden with the
+   * `ATSCM_PROJECT__LOGIN__USERNAME` env variable.
+   * @property {string} password The password to log in with. Can be overridden with the
+   * `ATSCM_PROJECT__LOGIN__PASSWORD` env variable.
+   */
+
+
+  static get login() {
+    if (this._env('LOGIN__USERNAME') || this._env('LOGIN__PASSWORD')) {
+      return {
+        username: this._env('LOGIN__USERNAME') || super.login.username,
+        password: this._env('LOGIN__PASSWORD') || super.login.password
+      };
+    }
+
+    return super.login;
+  }
+
+}
+
+exports.default = ProjectConfig;
+//# sourceMappingURL=ProjectConfig.js.map
---
+++ out/lib/server/scripts/CallMethodStream.js
@@ -0,0 +1,129 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _opcua_status_code = require("node-opcua/lib/datamodel/opcua_status_code");
+
+var _QueueStream = _interopRequireDefault(require("../QueueStream"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * A stream that calls an OPC-UA method for all input files.
+ * @abstract
+ */
+class CallMethodStream extends _QueueStream.default {
+  /**
+   * **Must be implemented in all subclasses:** The {@link NodeId} of the method to call.
+   * @type {NodeId} The method's id.
+   */
+  get methodId() {
+    throw new Error('Must be implemented in all subclasses');
+  }
+  /**
+   * The {@link NodeId} of the object from which the method should get called. Defaults to the value
+   * of {@link NodeId#parent} of {@link CallMethodStream#methodId}.
+   * @type {NodeId} The call-object's id.
+   */
+
+
+  get methodBaseId() {
+    return this.methodId.parent;
+  }
+  /**
+   * The input arguments the method should be called with for a file. Needs to be overridden by
+   * subclasses in most cases. Returning `null` indicates no method call is needed.
+   * @param {vinyl~File} file The file beeing processed.
+   * @return {?node-opcua~Variant[]} The resulting input arguments.
+   */
+
+
+  inputArguments(file) {
+    // eslint-disable-line no-unused-vars
+    return [];
+  }
+  /**
+   * Creates a call method request object for a file.
+   * @param {vinyl~File} file The file beeing processed.
+   * @return {?node-opcua~CallMethodRequest} The resulting call request.
+   */
+
+
+  callRequest(file) {
+    const args = this.inputArguments(file);
+
+    if (args === null) {
+      return null;
+    }
+
+    return {
+      objectId: this.methodBaseId,
+      methodId: this.methodId,
+      inputArguments: args
+    };
+  }
+  /**
+   * **Must be implemented by all subclasses:** If the method call returns a status code of
+   * *{@link node-opcua~StatusCodes}.Good*, this method decides if the output matches the expected
+   * results.
+   * @param {vinyl~File} file The file beeing processed.
+   * @param {node-opcua~Variant[]} outputArgs The output arguments.
+   * @param {function(err: Error)} callback Call this method with an error to indicate the method
+   * call didn't work as expected.
+   */
+
+
+  handleOutputArguments(file, outputArgs, callback) {
+    // eslint-disable-line no-unused-vars
+    throw new Error('Must be implemented in all subclasses');
+  }
+  /**
+   * Returns an error message specifically for the given file.
+   * @param {vinyl~File} file The file to generate the error message for.
+   * @return {string} The specific error message.
+   */
+
+
+  processErrorMessage(file) {
+    return `Error running ${this.methodId.toString()} for ${file.relative}`;
+  }
+  /**
+   * Performs an opcua method call for the given file.
+   * @param {vinyl~File} file The file being processed.
+   * @param {function(err: Error, status: node-opcua~StatusCodes, success: function)} handleErrors
+   * The error handler to call. See {@link QueueStream#processChunk} for details.
+   */
+
+
+  processChunk(file, handleErrors) {
+    try {
+      const request = this.callRequest(file);
+
+      if (!request) {
+        handleErrors(null, _opcua_status_code.StatusCodes.Good, done => done());
+        return;
+      }
+
+      this.session.call([request], (err, [result] = []) => {
+        if (err) {
+          handleErrors(err);
+        } else if (result.statusCode.value !== _opcua_status_code.StatusCodes.Good.value) {
+          handleErrors(err, result.statusCode, done => done());
+        } else {
+          this.handleOutputArguments(file, result.outputArguments, outputError => {
+            handleErrors(outputError, _opcua_status_code.StatusCodes.Good, done => done());
+          });
+        }
+      });
+    } catch (e) {
+      handleErrors(e);
+    }
+  }
+
+}
+
+exports.default = CallMethodStream;
+//# sourceMappingURL=CallMethodStream.js.map
