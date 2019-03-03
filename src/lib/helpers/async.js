e9508880581a58a24120a3be3a383905fa06ef75
Lukas Hechenberger <admin@ls-age.com>
2019-03-03 11:01:55 +0100

    docs: Add missing docs

--- src/lib/helpers/async.js
+++ src/lib/helpers/async.js
@@ -1,2 +1,8 @@
 /* eslint-disable import/prefer-default-export */
+
+/**
+ * Returns a promise that resolves after the given duration.
+ * @param {number} ms Milliseconds to delay.
+ * @return {Promise<void>} A promise resolved after the specified delay.
+ */
 export const delay = ms => new Promise((resolve) => setTimeout(() => resolve(), ms));
