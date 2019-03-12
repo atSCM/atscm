fe403ca86225138c509e6e7b89edd2a0e3fef0f0
CircleCI <circleci@circleci>
2019-03-12 07:26:13 +0000

    chore(release): Add 1.0.0-beta.26 release files [ci skip]

---
+++ out/typedef/atscm.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} NodeStream.BrowseResult
+ * @property {NodeId} nodeId The discovered node's id.
+ * @property {node-opcua~NodeClass} nodeClass The discovered node's class.
+ * @property {Map<String, NodeId[]>} references An object holding arrays of references from the
+ * discovered node to others, mapped by {@link node-opcua~ReferenceTypeId} keys.
+ */
+
+/**
+ * @typedef {NodeStream.BrowseResult} ReadStream.ReadResult
+ * @property {?node-opcua~DataValue} value For *Variable* nodes this property holds the read data
+ * value.
+ * @property {?Date} mtime For *Variable* nodes this property holds the timestamp the node's value
+ * last changed.
+ */
+"use strict";
+//# sourceMappingURL=atscm.js.map
