c109d765348b7253eb0a88e62c576cd782be7e4e
CircleCI <circleci@circleci>
2019-03-21 10:12:10 +0000

    chore(release): Add 1.0.0-beta.28 release files [ci skip]

---
+++ out/lib/helpers/mapping.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.sortReferences = sortReferences;
+
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * Sorts the given references before they are persisted to disk.
+ * @param {Object} references The references to be sorted.
+ */
+function sortReferences(references) {
+  return Object.keys(references).sort().reduce((sorted, key) => Object.assign(sorted, {
+    [key]: Array.isArray(references[key]) ? references[key].sort() : references[key]
+  }), {});
+}
+//# sourceMappingURL=mapping.js.map
