f101c9e050bf785465f9cf591ce28f91a7e68b4f
LukasHechenberger <admin@ls-age.com>
2019-04-28 14:56:31 +0200

    test: Test mapping of folders inside displays

--- test/integration/mapping.spec.js
+++ test/integration/mapping.spec.js
@@ -1,10 +1,17 @@
 import { expectCorrectMapping } from '../helpers/atscm';
 
-describe.skip('Mapping', function() {
-  context('when resource directory with dot in name', function() {
+describe('Mapping', function() {
+  context.skip('when resource directory with dot in name', function() {
     expectCorrectMapping('mapping/dot-in-dirname', {
       path: 'SYSTEM.LIBRARY.PROJECT.RESOURCES/DataTables/Bootstrap-4-4.0.0/css',
       name: 'bootstrap.min.css',
+    });
+  });
+
+  context('a display containing a folder', function() {
+    expectCorrectMapping('mapping/folder-in-display', {
+      path: 'AGENT.DISPLAYS',
+      name: 'FolderInDisplay',
     });
   });
 });
