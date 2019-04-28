d07b34b35341167142b1f943d9107d7db0c8af6b
CircleCI <circleci@circleci>
2019-04-28 12:27:43 +0000

    chore(release): Add 1.0.0-beta.31 release files [ci skip]

---
+++ out/api.js
@@ -0,0 +1,242 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.callMethod = callMethod;
+exports.callScript = callScript;
+exports.createNode = createNode;
+exports.addReferences = addReferences;
+exports.readNode = readNode;
+exports.writeNode = writeNode;
+
+var _nodeclass = require("node-opcua/lib/datamodel/nodeclass");
+
+var _variant = require("node-opcua/lib/datamodel/variant");
+
+var _opcua_status_code = require("node-opcua/lib/datamodel/opcua_status_code");
+
+var _Session = _interopRequireDefault(require("./lib/server/Session"));
+
+var _NodeId = _interopRequireDefault(require("./lib/model/opcua/NodeId"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Helpers
+
+/**
+ * Creates a callback that calls `resolve` on success and `reject` on error.
+ * @param {function(result: any): void} resolve The resolve callback.
+ * @param {function(error: Error): void} reject The reject callback.
+ * @example
+ * // `aCallbackFn` is a function that accepts a node-style callback as the last argument
+ * const promise = new Promise(
+ *   (resolve, reject) => aCallbackFn('other', 'args', promisifiedCallback(resolve, reject)
+ * );
+ */
+function promisifiedCallback(resolve, reject) {
+  return (err, result) => {
+    if (err) {
+      return reject(err);
+    }
+
+    return resolve(result);
+  };
+}
+/**
+ * Promisifies a async function that would otherwise require a callback.
+ * @param {function(cb: function(error: Error, result: any)):Promise<any>} call A function that
+ * accepts a callback and performs the async action to wrap.
+ */
+
+
+function promisified(call) {
+  return new Promise((resolve, reject) => call(promisifiedCallback(resolve, reject)));
+}
+/**
+ * Creates a session, runs `action` and closes the session.
+ * @param {function(session: Session): Promise<any>} action The action to run a session.
+ */
+
+
+async function withSession(action) {
+  const session = await _Session.default.create();
+  let result = null;
+  let error = null;
+
+  try {
+    result = await action(session);
+  } catch (e) {
+    error = e;
+  }
+
+  await _Session.default.close(session);
+
+  if (error) {
+    throw error;
+  }
+
+  return result;
+} // Methods / Scripts
+
+/**
+ * Calls an OPC-UA method on the server.
+ * @param {NodeId} methodId The method's id.
+ * @param {Array<Variant>} args The arguments to pass.
+ */
+
+
+function callMethod(methodId, args = []) {
+  return withSession(session => promisified(cb => session.call([{
+    objectId: methodId.parent,
+    methodId,
+    inputArguments: args
+  }], cb))).then(([result] = []) => result);
+}
+/**
+ * Calls a server script on the server.
+ * @param {NodeId} scriptId The script's id.
+ * @param {Object} parameters The parameters to pass, given as a map of Variants, like
+ * `{ name: { ... } }`.
+ */
+
+
+function callScript(scriptId, parameters = {}) {
+  return callMethod(new _NodeId.default('AGENT.SCRIPT.METHODS.callScript'), [{
+    dataType: _variant.DataType.NodeId,
+    value: scriptId
+  }, {
+    dataType: _variant.DataType.NodeId,
+    value: scriptId.parent
+  }, {
+    dataType: _variant.DataType.String,
+    arrayType: _variant.VariantArrayType.Array,
+    value: Object.keys(parameters)
+  }, {
+    dataType: _variant.DataType.Variant,
+    arrayType: _variant.VariantArrayType.Array,
+    value: Object.values(parameters)
+  }]);
+}
+/**
+ * Creates a new Node on the server.
+ * @param {NodeId} nodeId The new node's id.
+ * @param {Object} options The options to use.
+ * @param {string} options.name The node's name.
+ * @param {NodeId} [options.parentNodeId] The node's parent, defaults to the calculated parent
+ * (`Test` for `Test.Child`).
+ * @param {node-opcua~NodeClass} [options.nodeClass] The node's class, defaults so
+ * `node-opcua~NodeClass.Variable`.
+ * @param {NodeId} [options.typeDefinition] The node's type definition, must be provided for
+ * non-variable nodes.
+ * @param {NodeId} [options.modellingRule] The node's modelling rule.
+ * @param {string} [options.reference] Name of the type of the node's reference to it's parent.
+ * @param {node-opcua~Variant} [options.value] The node's value, required for all variable nodes.
+ */
+
+
+function createNode(nodeId, {
+  name,
+  parentNodeId = nodeId.parent,
+  nodeClass = _nodeclass.NodeClass.Variable,
+  typeDefinition = new _NodeId.default('ns=0;i=62'),
+  modellingRule,
+  reference,
+  value
+}) {
+  const variableOptions = nodeClass === _nodeclass.NodeClass.Variable ? {
+    dataType: value.dataType.value,
+    valueRank: value.arrayType ? value.arrayType.value : _variant.VariantArrayType.Scalar.value,
+    value: value.value
+  } : {};
+  return callScript(new _NodeId.default('SYSTEM.LIBRARY.ATVISE.SERVERSCRIPTS.atscm.CreateNode'), {
+    paramObjString: {
+      dataType: _variant.DataType.String,
+      value: JSON.stringify(Object.assign({
+        nodeId,
+        browseName: name,
+        parentNodeId: parentNodeId || nodeId.parent,
+        nodeClass: nodeClass.value,
+        typeDefinition,
+        modellingRule,
+        reference
+      }, variableOptions))
+    }
+  });
+}
+/**
+ * Adds references to a node.
+ * @param {NodeId} nodeId The node to add the references to.
+ * @param {Object} references The references to add.
+ * @return {Promise} Resolved once the references were added.
+ * @example <caption>Add a simple reference</caption>
+ * import { ReferenceTypeIds } from 'node-opcua/lib/opcua_node_ids';
+ *
+ * addReferences('AGENT.DISPLAYS.Main', {
+ *   [47]: ['VariableTypes.ATVISE.Display'],
+ *   // equals:
+ *   [ReferenceTypeIds.HasTypeDefinition]: ['VariableTypes.ATVISE.Display'],
+ * })
+ *   .then(() => console.log('Done!'))
+ *   .catch(console.error);
+ */
+
+
+function addReferences(nodeId, references) {
+  return callScript(new _NodeId.default('SYSTEM.LIBRARY.ATVISE.SERVERSCRIPTS.atscm.AddReferences'), {
+    paramObjString: {
+      dataType: _variant.DataType.String,
+      value: JSON.stringify({
+        nodeId,
+        references: Object.entries(references).map(([type, items]) => ({
+          referenceIdValue: parseInt(type, 10),
+          items
+        }))
+      })
+    }
+  });
+} // Reading/Writing
+
+/**
+ * Reads a single node's value.
+ * @param {NodeId} nodeId The node to read.
+ * @return {Promise<any>} The read value.
+ */
+
+
+async function readNode(nodeId) {
+  return withSession(session => promisified(cb => session.readVariableValue(nodeId, cb))).then(({
+    value,
+    statusCode
+  }) => {
+    if (statusCode !== _opcua_status_code.StatusCodes.Good) {
+      throw Object.assign(new Error(statusCode.description), {
+        nodeId,
+        statusCode
+      });
+    }
+
+    return value;
+  });
+}
+/**
+ * Writes a single node's value.
+ * @param {NodeId} nodeId The node to write.
+ * @param {Variant} value The value to write.
+ * @return {Promise<node-opcua~StatusCodes} The operation status result.
+ */
+
+
+function writeNode(nodeId, value) {
+  return withSession(session => promisified(cb => session.writeSingleNode(nodeId, value, cb))).then(statusCode => {
+    if (statusCode !== _opcua_status_code.StatusCodes.Good) {
+      throw Object.assign(new Error(statusCode.description), {
+        nodeId,
+        statusCode
+      });
+    }
+
+    return statusCode;
+  });
+}
+//# sourceMappingURL=api.js.map
---
+++ out/transform/ScriptTransformer.js
@@ -0,0 +1,331 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.QuickDynamicTransformer = exports.ServerscriptTransformer = exports.AtviseScriptTransformer = void 0;
+
+var _gulplog = _interopRequireDefault(require("gulplog"));
+
+var _variant = require("node-opcua/lib/datamodel/variant");
+
+var _XMLTransformer = _interopRequireDefault(require("../lib/transform/XMLTransformer"));
+
+var _xml = require("../lib/helpers/xml");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+/**
+ * A transformer that splits atvise scripts and quick dynamics into a code file and a .json file
+ * containing parameters and metadata.
+ */
+class AtviseScriptTransformer extends _XMLTransformer.default {
+  /**
+   * Extracts a script's metadata.
+   * @param {Object} document The parsed xml document to process.
+   * @return {Object} The metadata found.
+   */
+  processMetadata(document) {
+    const config = {};
+    const metaTag = (0, _xml.findChild)(document, 'metadata');
+
+    if (!metaTag || !metaTag.elements) {
+      return config;
+    }
+
+    metaTag.elements.forEach(child => {
+      if (child.type !== 'element') {
+        return;
+      }
+
+      switch (child.name) {
+        case 'icon':
+          config.icon = Object.assign({
+            content: (0, _xml.textContent)(child) || ''
+          }, child.attributes);
+          break;
+
+        case 'visible':
+          config.visible = Boolean(parseInt((0, _xml.textContent)(child), 10));
+          break;
+
+        case 'title':
+          config.title = (0, _xml.textContent)(child);
+          break;
+
+        case 'description':
+          config.description = (0, _xml.textContent)(child);
+          break;
+
+        default:
+          {
+            if (!config.metadata) {
+              config.metadata = {};
+            }
+
+            const value = (0, _xml.textContent)(child);
+
+            if (config.metadata[child.name]) {
+              if (!Array.isArray(config.metadata[child.name])) {
+                config.metadata[child.name] = [config.metadata[child.name]];
+              }
+
+              config.metadata[child.name].push(value);
+            } else {
+              config.metadata[child.name] = (0, _xml.textContent)(child);
+            }
+
+            if (!['longrunning'].includes(child.name)) {
+              _gulplog.default.debug(`Generic metadata element '${child.name}'`); // FIXME:  at ${node.nodeId}
+
+            }
+
+            break;
+          }
+      }
+    });
+    return config;
+  }
+  /**
+   * Extracts a script's parameters.
+   * @param {Object} document The parsed xml document to process.
+   * @return {Object[]} The parameters found.
+   */
+
+
+  processParameters(document) {
+    const paramTags = (0, _xml.findChildren)(document, 'parameter');
+
+    if (!paramTags.length) {
+      return undefined;
+    }
+
+    return paramTags.map(({
+      attributes,
+      elements
+    }) => {
+      const param = Object.assign({}, attributes); // Handle relative parameter targets
+
+      if (attributes.relative === 'true') {
+        param.target = {};
+        const target = (0, _xml.findChild)(elements[0], ['Elements', 'RelativePathElement', 'TargetName']);
+
+        if (target) {
+          const [index, name] = ['NamespaceIndex', 'Name'].map(tagName => (0, _xml.textContent)((0, _xml.findChild)(target, tagName)));
+          const parsedIndex = parseInt(index, 10);
+          param.target = {
+            namespaceIndex: isNaN(parsedIndex) ? 1 : parsedIndex,
+            name
+          };
+        }
+      }
+
+      return param;
+    });
+  }
+  /**
+   * Splits a node into multiple source nodes.
+   * @param {Node} node A server node.
+   * @param {Object} context The current transform context.
+   */
+
+
+  async transformFromDB(node, context) {
+    if (!this.shouldBeTransformed(node)) {
+      return undefined;
+    }
+
+    if (node.arrayType !== _variant.VariantArrayType.Scalar) {
+      // FIXME: Instead of throwing we could simply pass the original node to the callback
+      throw new Error('Array of scripts not supported');
+    }
+
+    const xml = this.decodeContents(node);
+
+    if (!xml) {
+      throw new Error('Error parsing script');
+    }
+
+    const document = (0, _xml.findChild)(xml, 'script');
+
+    if (!document) {
+      throw new Error(`Empty document at ${node.nodeId}`);
+    } // Extract metadata and parameters
+
+
+    const config = _objectSpread({}, this.processMetadata(document), {
+      parameters: this.processParameters(document)
+    }); // Write config file
+
+
+    const configFile = this.constructor.splitFile(node, '.json');
+    configFile.value = {
+      dataType: _variant.DataType.String,
+      arrayType: _variant.VariantArrayType.Scalar,
+      value: JSON.stringify(config, null, '  ')
+    };
+    context.addNode(configFile); // Write JavaScript file
+
+    const codeNode = (0, _xml.findChild)(document, 'code');
+    const scriptFile = this.constructor.splitFile(node, '.js');
+    scriptFile.value = {
+      dataType: _variant.DataType.String,
+      arrayType: _variant.VariantArrayType.Scalar,
+      value: (0, _xml.textContent)(codeNode) || ''
+    };
+    context.addNode(scriptFile);
+    return super.transformFromDB(node);
+  }
+  /**
+   * Inlines the passed source nodes to the given container node.
+   * @param {Node} node The container node.
+   * @param {{ [ext: string]: Node }} sources The source nodes to inline.
+   */
+
+
+  combineNodes(node, sources) {
+    const configFile = sources['.json'];
+    let config = {};
+
+    if (configFile) {
+      try {
+        config = JSON.parse(configFile.stringValue);
+      } catch (e) {
+        throw new Error(`Error parsing JSON in ${configFile.relative}: ${e.message}`);
+      }
+    }
+
+    const scriptFile = sources['.js'];
+    let code = '';
+
+    if (scriptFile) {
+      code = scriptFile.stringValue;
+    }
+
+    const document = (0, _xml.createElement)('script', []);
+    const result = {
+      elements: [document]
+    }; // Insert metadata
+
+    const meta = [];
+
+    if (node.isQuickDynamic) {
+      // - Icon
+      if (config.icon) {
+        const icon = config.icon.content;
+        delete config.icon.content;
+        meta.push((0, _xml.createElement)('icon', [(0, _xml.createTextNode)(icon)], config.icon));
+      } // - Other fields
+
+
+      if (config.visible !== undefined) {
+        meta.push((0, _xml.createElement)('visible', [(0, _xml.createTextNode)(`${config.visible ? 1 : 0}`)]));
+      }
+
+      if (config.title !== undefined) {
+        meta.push((0, _xml.createElement)('title', [(0, _xml.createTextNode)(config.title)]));
+      }
+
+      if (config.description !== undefined) {
+        meta.push((0, _xml.createElement)('description', [(0, _xml.createTextNode)(config.description)]));
+      }
+    } // - Additional fields
+
+
+    if (config.metadata !== undefined) {
+      Object.entries(config.metadata).forEach(([name, value]) => {
+        (Array.isArray(value) ? value : [value]).forEach(v => meta.push((0, _xml.createElement)(name, [(0, _xml.createTextNode)(v)])));
+      });
+    }
+
+    if (node.isQuickDynamic || meta.length) {
+      document.elements.push((0, _xml.createElement)('metadata', meta));
+    } // Insert parameters
+
+
+    if (config.parameters) {
+      config.parameters.forEach(attributes => {
+        let elements; // Handle relative parameter targets
+
+        if (attributes.relative === 'true' && attributes.target) {
+          const {
+            namespaceIndex,
+            name
+          } = attributes.target;
+          const targetElements = (0, _xml.createElement)('Elements');
+          elements = [(0, _xml.createElement)('RelativePath', [targetElements])];
+
+          if (name !== undefined) {
+            targetElements.elements = [(0, _xml.createElement)('RelativePathElement', [(0, _xml.createElement)('TargetName', [(0, _xml.createElement)('NamespaceIndex', [(0, _xml.createTextNode)(`${namespaceIndex}`)]), (0, _xml.createElement)('Name', [(0, _xml.createTextNode)(`${name}`)])])])];
+          } // eslint-disable-next-line no-param-reassign
+
+
+          delete attributes.target;
+        }
+
+        document.elements.push((0, _xml.createElement)('parameter', elements, attributes));
+      });
+    } // Insert script code
+
+
+    document.elements.push((0, _xml.createElement)('code', [(0, _xml.createCDataNode)(code)])); // eslint-disable-next-line no-param-reassign
+
+    node.value.value = this.encodeContents(result);
+  }
+
+}
+/**
+ * A transformer that splits atvise server scripts into multiple files.
+ */
+
+
+exports.AtviseScriptTransformer = AtviseScriptTransformer;
+
+class ServerscriptTransformer extends AtviseScriptTransformer {
+  /** The container's extension. */
+  static get extension() {
+    return '.script';
+  }
+  /**
+   * Returns `true` for all script nodes.
+   * @param {Node} node The node to check.
+   * @return {boolean} If the node is a server script.
+   */
+
+
+  shouldBeTransformed(node) {
+    return node.isVariable && node.isScript;
+  }
+
+}
+/**
+ * A transformer that splits atvise quickdynamics into multiple files.
+ */
+
+
+exports.ServerscriptTransformer = ServerscriptTransformer;
+
+class QuickDynamicTransformer extends AtviseScriptTransformer {
+  /** The container's extension. */
+  static get extension() {
+    return '.qd';
+  }
+  /**
+   * Returns `true` for all nodes containing quick dynamics.
+   * @param {Node} node The node to check.
+   * @return {boolean} If the node is a quick dynamic.
+   */
+
+
+  shouldBeTransformed(node) {
+    return node.isVariable && node.isQuickDynamic;
+  }
+
+}
+
+exports.QuickDynamicTransformer = QuickDynamicTransformer;
+//# sourceMappingURL=ScriptTransformer.js.map
