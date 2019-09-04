7319b1d0db6448e7627e0c52dae882f3ffed8252
LukasHechenberger <admin@ls-age.com>
2019-09-04 11:59:23 +0200

    test: Use nes unexpected plugin syntax

--- test/expect.js
+++ test/expect.js
@@ -2,7 +2,7 @@ import expect from 'unexpected';
 import unexpectedStream from 'unexpected-stream';
 import unexpectedSinon from 'unexpected-sinon';
 
-expect.installPlugin(unexpectedStream);
-expect.installPlugin(unexpectedSinon);
-
-export default expect;
+export default expect
+  .clone()
+  .use(unexpectedStream)
+  .use(unexpectedSinon);
