51930a3a75c92051c4d835943e73e2150fd6bdbd
Lukas Hechenberger <admin@ls-age.com>
2019-12-10 16:08:52 +0100

    test: Cleanup after testing against issue 345

--- test/integration/issues/issue-345.spec.js
+++ test/integration/issues/issue-345.spec.js
@@ -3,15 +3,16 @@ import { spy } from 'sinon';
 import { readdir } from 'fs-extra';
 import expect from '../../expect';
 import { tmpDir } from '../../helpers/util';
-import { importSetup, pull } from '../../helpers/atscm';
+import { importSetup, pull, deleteNode } from '../../helpers/atscm';
 
 describe('Issue #345 (https://github.com/atSCM/atscm/issues/345)', function() {
   const setup = 'issue-345';
+  let nodeIds;
 
   it('pull should not error on empty script', async function() {
     const nodeNames = await importSetup(setup, 'EmptyScript');
     const nodePaths = ['SYSTEM.LIBRARY.PROJECT.MENUSCRIPTS'];
-    const nodeIds = nodeNames.map((nodeName, i) => {
+    nodeIds = nodeNames.map((nodeName, i) => {
       const path = nodePaths[i];
       const divider = path.match(/RESOURCES/) ? '/' : '.';
       return `${path}${divider}${nodeName}`;
@@ -31,4 +32,6 @@ describe('Issue #345 (https://github.com/atSCM/atscm/issues/345)', function() {
     expect(warnSpy, 'was called once');
     return expect(warnSpy, 'to have a call satisfying', { args: [/is empty/] });
   });
+
+  after(() => nodeIds.forEach(id => deleteNode(id)));
 });
