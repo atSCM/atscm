25bdd58ebfe3beae9fc10c9fa98691c71ac8aff9
LukasHechenberger <admin@ls-age.com>
2020-03-12 23:09:36 +0100

    style: Update init template types

--- res/init/templates/lang/ts/Atviseproject.ts
+++ res/init/templates/lang/ts/Atviseproject.ts
@@ -5,40 +5,33 @@ import { Atviseproject } from 'atscm';
  */
 export default class {{pascalcase name}} extends Atviseproject {
 
-    /**
-     * The atvise-server's host
-     * @type {String}
-     */
-    static get host(): String {
-        return '{{atviseHost}}';
-    }
+  /**
+   * The atvise-server's host.
+   */
+  static get host() {
+    return '{{atviseHost}}';
+  }
 
-    /**
-     * The atvise-server ports to use.
-     * @type {Object}
-     * @property {Number} opc The OPC-UA port the atvise-server runs on.
-     * @property {Number} http The HTTP port the atvise-server can be reached at.
-     */
-    static get port(): Object {
-        return {
-            opc: {{atvisePortOpc}},
-            http: {{atvisePortHttp}},
-        };
-    }
-    {{#if useLogin}}
+  /**
+   * The atvise-server ports to use.
+   */
+  static get port() {
+    return {
+      opc: {{atvisePortOpc}},
+      http: {{atvisePortHttp}},
+    };
+  }
+  {{#if useLogin}}
 
-    /**
-     * The login to use. Return `false` if no login is required.
-     * @type {Boolean|Object}
-     * @property {String} username The username to log in with.
-     * @property {String} password The password to log in with.
-     */
-    static get login(): Object {
-        return {
-            username: '{{atviseUser}}',
-            password: '{{atvisePassword}}',
-        };
-    }
-    {{/if}}
+  /**
+   * The login to use. Return `false` if no login is required.
+   */
+  static get login() {
+    return {
+      username: '{{atviseUser}}',
+      password: '{{atvisePassword}}',
+    };
+  }
+  {{/if}}
 
 }
