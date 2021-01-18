fc2c0e9560811bc7efbdc3899da7230ae690cb3a
CircleCI <circleci@circleci>
2021-01-18 20:37:19 +0000

    chore(release): Add atscm 1.1.1 release files [ci skip]

---
+++ packages/atscm/out/types/src/Gulpfile.d.ts
@@ -0,0 +1,3 @@
+export { default as pull } from "./tasks/pull";
+export { default as push } from "./tasks/push";
+export { default as watch } from "./tasks/watch";
---
+++ packages/atscm/out/types/src/lib/transform/PartialTransformer.d.ts
@@ -0,0 +1,30 @@
+/**
+ * A transformer that transforms only some of the files read.
+ * @abstract
+ * @example <caption>Minimum implementation</caption>
+ * class MyTransformer extends PartialTransformer {
+ *   shouldBeTransformed(node) {
+ *     return true; // or false
+ *   }
+ *   transformFromFilesystem(state, context) {
+ *     if (!this.shouldBeTransformed(state.node)) { return; }
+ *
+ *     // Apply actual transformation
+ *   }
+ * }
+ */
+export default class ModernPartialTransformer extends Transformer {
+    constructor({ direction }?: {
+        direction?: {
+            FromDB: string;
+            FromFilesystem: string;
+        };
+    });
+    /**
+     * `true` if `file` should be transformed.
+     * @param {AtviseFile} file The file to transform or not.
+     * @abstract
+     */
+    shouldBeTransformed(file: any): void;
+}
+import Transformer from "./Transformer";
