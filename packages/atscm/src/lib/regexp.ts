6d18afefe11adabe586ac75f29dbde7b8da1d852
LukasHechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2021-01-18 20:53:15 +0100

    docs: Add missing annotations

--- packages/atscm/src/lib/regexp.ts
+++ packages/atscm/src/lib/regexp.ts
@@ -1,10 +1,16 @@
+/** Special characters in regular expressions. */
 export const specialChars = ['[', '\\', '^', '$', '.', '|', '?', '*', '+', '(', ')', ']'];
 
+/** A regular expression that matches all special characters in regular expressions. */
 export const specialCharsRegExp = new RegExp(
   `(${specialChars.map((c) => `\\${c}`).join('|')})`,
   'g'
 );
 
+/**
+ * Returns a string with all special regular expression characters escaped.
+ * @param source The string to escape.
+ */
 export function escapeForRegExp(source: string) {
   return source.replace(specialCharsRegExp, '\\$1');
 }
